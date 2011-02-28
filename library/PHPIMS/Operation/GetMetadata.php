79f4823f8b698b8c140f11a68598a8fa42c3f56c
Christer Edvartsen <cogo@starzinger.net>
2011-02-28 21:04:35 +0100

    Forgot to add the operation class

---
+++ library/PHPIMS/Operation/GetMetadata.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * PHPIMS
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package PHPIMS
+ * @subpackage Operations
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+
+/**
+ * Get metadata operation
+ *
+ * This operation will return all metadata associated with a single image
+ *
+ * @package PHPIMS
+ * @subpackage Operations
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+class PHPIMS_Operation_GetMetadata extends PHPIMS_Operation_Abstract {
+    /**
+     * Execute the operation
+     *
+     * Operations must implement this method and return a PHPIMS_Server_Response object to return
+     * to the client.
+     *
+     * @return PHPIMS_Server_Response
+     * @throws PHPIMS_Operation_Exception
+     */
+    public function exec() {
+        try {
+            $data = $this->getDatabase()->getImageMetadata($this->getHash());
+        } catch (PHPIMS_Database_Exception $e) {
+            throw new PHPIMS_Operation_Exception('Could not get metadata', 0, $e);
+        }
+
+        $response = new PHPIMS_Server_Response(200);
+        $response->setData($data);
+
+        return $response;
+    }
+}
