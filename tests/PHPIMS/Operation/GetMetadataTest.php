537855de7d6c737e911e0497d9de4e748b15bae9
Christer Edvartsen <cogo@starzinger.net>
2011-03-03 07:21:53 +0100

    Added some more tests for the implemented operations

---
+++ tests/PHPIMS/Operation/GetMetadataTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * PHPIMS
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package PHPIMS
+ * @subpackage Unittests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+
+/**
+ * @package PHPIMS
+ * @subpackage Unittests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+class PHPIMS_Operation_GetMetadataTest extends PHPUnit_Framework_TestCase {
+    /**
+     * Operation instance
+     *
+     * @var PHPIMS_Operation_GetMetadata
+     */
+    protected $operation = null;
+
+    /**
+     * Hash value sent to the database driver
+     *
+     * @var string
+     */
+    protected $hash = 'some hash value';
+
+    /**
+     * Set up method
+     */
+    public function setUp() {
+        $this->operation = new PHPIMS_Operation_GetMetadata($this->hash);
+    }
+
+    /**
+     * Tear down method
+     */
+    public function tearDown() {
+        $this->operation = null;
+    }
+
+    /**
+     * @expectedException PHPIMS_Operation_Exception
+     * @expectedExceptionMessage Could not get metadata
+     */
+    public function testExecWhenDatabaseFails() {
+        $database = $this->getMockForAbstractClass('PHPIMS_Database_Driver_Abstract');
+        $database->expects($this->once())->method('getImageMetadata')->with($this->hash)->will($this->throwException(new PHPIMS_Database_Exception()));
+        $this->operation->setDatabase($database);
+
+        $this->operation->exec();
+    }
+
+    public function testSuccessfullExec() {
+        $data = array(
+            'foo' => 'bar',
+            'bar' => 'foo',
+        );
+        $database = $this->getMockForAbstractClass('PHPIMS_Database_Driver_Abstract');
+        $database->expects($this->once())->method('getImageMetadata')->with($this->hash)->will($this->returnValue($data));
+        $this->operation->setDatabase($database);
+
+        $response = $this->operation->exec();
+        $this->assertInstanceOf('PHPIMS_Server_Response', $response);
+        $this->assertSame($data, $response->getData());
+    }
+}
