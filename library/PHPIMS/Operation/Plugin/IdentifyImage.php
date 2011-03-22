dcbfc340a356278d37f5af63fe140906f5b472d3
Christer Edvartsen <cogo@starzinger.net>
2011-03-22 18:56:38 +0100

    Added a plugin that can identify images in the response buffer and set the correct content-type header

---
+++ library/PHPIMS/Operation/Plugin/IdentifyImage.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * PHPIMS
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package PHPIMS
+ * @subpackage OperationPlugin
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+
+/**
+ * Identify image plugin
+ *
+ * This plugin will identify the mime type of the current image in the response object (using the
+ * fileinfo extension).
+ *
+ * @package PHPIMS
+ * @subpackage OperationPlugin
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+class PHPIMS_Operation_Plugin_IdentifyImage extends PHPIMS_Operation_Plugin_Abstract {
+    /**
+     * @see PHPIMS_Operation_Plugin_Abstract::postExec
+     */
+    public function postExec() {
+        $response = $this->getOperation()->getResponse();
+
+        $finfo = new finfo(FILEINFO_MIME);
+        $response->setContentType($finfo->buffer($response->getRawData()));
+    }
+}
