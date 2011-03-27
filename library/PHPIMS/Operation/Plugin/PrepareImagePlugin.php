3b90e6fefed451c34b2edb433efb13d981e0b4cf
Christer Edvartsen <cogo@starzinger.net>
2011-03-27 13:04:56 +0200

    Changed the order of plugin execution and some other small fixes that had broken in the refactoring

--- library/PHPIMS/Operation/Plugin/PrepareImagePlugin.php
+++ library/PHPIMS/Operation/Plugin/PrepareImagePlugin.php
@@ -48,18 +48,25 @@ class PHPIMS_Operation_Plugin_PrepareImagePlugin extends PHPIMS_Operation_Plugin
      * @see PHPIMS_Operation_Plugin_Abstract::$events
      */
     static public $events = array(
-        'addImagePreExec' => 101,
+        'addImagePreExec' => 100,
     );
 
     /**
      * @see PHPIMS_Operation_Plugin_Abstract::exec()
      */
     public function exec() {
+        // Make sure there is an image attached
+        if (empty($_FILES)) {
+            throw new PHPIMS_Operation_Exception('No image attached', 400);
+        }
+
         $image = $this->getOperation()->getImage();
 
         $imagePath = $_FILES['file']['tmp_name'];
-        $actualHash = md5_file($imagePath) . '.' . $image->getExtension();
-        $hashFromRequest = $this->getOperation()->getHash();
+        $actualHash = md5_file($imagePath);
+
+        // Get hash from request without the extension
+        $hashFromRequest = substr($this->getOperation()->getHash(), 0, 32);
 
         if ($actualHash !== $hashFromRequest) {
             throw new PHPIMS_Operation_Plugin_Exception('Hash mismatch', 400);
@@ -68,6 +75,7 @@ class PHPIMS_Operation_Plugin_PrepareImagePlugin extends PHPIMS_Operation_Plugin
         $image->setFilename($_FILES['file']['name'])
               ->setFilesize($_FILES['file']['size'])
               ->setMetadata($_POST)
-              ->setHash($hashFromRequest);
+              ->setHash($hashFromRequest)
+              ->setBlob(file_get_contents($imagePath));
     }
 }
