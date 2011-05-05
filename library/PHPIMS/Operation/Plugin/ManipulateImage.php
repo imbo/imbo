cee6728f8f7c22c84feffcc28067936eaed2d205
Christer Edvartsen <cogo@starzinger.net>
2011-05-05 21:30:51 +0200

    Removed some phpdoc

--- library/PHPIMS/Operation/Plugin/ManipulateImage.php
+++ library/PHPIMS/Operation/Plugin/ManipulateImage.php
@@ -50,55 +50,7 @@ use \Imagine\ImageInterface as ImagineImage;
  * Manipulate image plugin
  *
  * This plugin enables image manipulation using query parameters. Users can specify as many
- * transformations they want. Transformations will be applied in the order they are given. Each
- * transformation is added using the following format:
- *
- * <pre>t[]=transformation:param=value[,param=value[, ... ]]</pre>
- *
- * Valid values for <transformation> and their respective parameters are:
- *
- * <ul>
- *   <li>
- *     <pre>resize</pre>
- *     This transformation will resize the image. Two parameters are supported and at least one of them must be supplied to apply this transformation:
- *     <ul>
- *       <li><pre>(int) width</pre>The width of the resulting image in pixels. If not specified the width will be calculated using the same ratio as the original image.</li>
- *       <li><pre>(int) height</pre>The height of the resulting image in pixels. If not specified the height will be calculated using the same ratio as the original image.</li>
- *     </ul>
- *   </li>
- *   <li>
- *     <pre>crop</pre>
- *     This transformation will crop the image. All four arguments are required.
- *     <ul>
- *       <li><pre>(int) x</pre>The X coordinate of the cropped region's top left corner</li>
- *       <li><pre>(int) y</pre>The Y coordinate of the cropped region's top left corner</li>
- *       <li><pre>(int) width</pre>The width of the crop</li>
- *       <li><pre>(int) height</pre>The height of the crop</li>
- *     </ul>
- *   </li>
- *   <li>
- *     <pre>rotate</pre>
- *     Use this transformation to rotate the image.
- *     <ul>
- *       <li><pre>(int) angle</pre>The number of degrees to rotate the image</li>
- *       <li><pre>(string) bg</pre>Background color in hexadecimal. Defaults to "000000"</li>
- *     </ul>
- *   </li>
- *   <li>
- *     <pre>border</pre>
- *     If you want to add a border around the image, use this transformation.
- *     <ul>
- *       <li><pre>(string) color</pre>Color in hexadecimal. Defaults to "000000" (also supports short variants, for instance '000' or 'f00')</li>
- *       <li><pre>(int) width</pre>Width of the border on the left and right sides of the image. Defaults to 1</li>
- *       <li><pre>(int) height</pre>Height of the border on the top and bottoms sides of the image. Defaults to 1</li>
- *     </ul>
- *   </li>
- * </ul>
- *
- * Examples of transformations:
- *
- * - <pre>?t[]=border&t[]=resize:width=100,height=50</pre>
- * - <pre>?t[]=crop:x=10,y=20,width=100,height=50&t[]=rotate:angle=45</pre>
+ * transformations they want. Transformations will be applied in the order they are given.
  *
  * @package PHPIMS
  * @subpackage OperationPlugin
