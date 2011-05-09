96e13adc8e09e0a81b2bcee0f2999a4e8ba71719
Mats Lindh <mats@derdubor.no>
2011-05-09 16:08:00 +0200

    Set CURL_CUSTOMREQUEST to the expected request type for each request, as it will otherwise keep the request type for subsequent requests (such as sending a DELETE instead of a GET if you've previously DELETE'd a resource).
    I have not been able to find a proper way of resetting this in PHP (the libcurl documentation says to set it to NULL, but this apparently gets overrun somewhere in PHP's usage of libcurl. Need to investigate later.
    
    Remove stray var_dump from debugging. Sorry. :(

--- library/PHPIMS/Client/Driver/Curl.php
+++ library/PHPIMS/Client/Driver/Curl.php
@@ -107,6 +107,7 @@ class Curl implements DriverInterface {
         curl_setopt_array($this->curlHandle, array(
             CURLOPT_POST       => true,
             CURLOPT_POSTFIELDS => $postFields,
+            CURLOPT_CUSTOMREQUEST => 'POST',
         ));
 
         return $this->request($url);
@@ -118,6 +119,7 @@ class Curl implements DriverInterface {
     public function get($url) {
         curl_setopt_array($this->curlHandle, array(
             CURLOPT_HTTPGET => true,
+            CURLOPT_CUSTOMREQUEST => 'GET',
         ));
 
         return $this->request($url);
@@ -129,6 +131,7 @@ class Curl implements DriverInterface {
     public function head($url) {
         curl_setopt_array($this->curlHandle, array(
             CURLOPT_NOBODY => true,
+            CURLOPT_CUSTOMREQUEST => 'HEAD',
         ));
 
         return $this->request($url);
