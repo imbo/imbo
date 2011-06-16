033128ca10d5fd3c7fb382ed4bcfe36666f75194
Christer Edvartsen <cogo@starzinger.net>
2011-06-16 07:51:07 +0200

    Made drivers adhere to the new interfaces. The MySQL database driver does not yet use the new parameters. @matslindh will hopefully fix this.

--- library/PHPIMS/Database/Driver/MySQL.php
+++ library/PHPIMS/Database/Driver/MySQL.php
@@ -108,7 +108,7 @@ class MySQL implements DriverInterface {
     /**
      * @see PHPIMS\Database\DriverInterface::insertImage()
      */
-    public function insertImage($imageIdentifier, Image $image) {
+    public function insertImage($publicKey, $imageIdentifier, Image $image) {
         try {
             // See if the image already exists
             if ($this->imageExists($imageIdentifier)) {
@@ -127,10 +127,10 @@ class MySQL implements DriverInterface {
                     :mime,
                     :added,
                     :width,
-                    :height                
+                    :height
                 )
             ");
-            
+
             $insertImageStatement->execute(array(
                 ':name'            => $image->getFilename(),
                 ':size'            => $image->getFilesize(),
@@ -141,7 +141,7 @@ class MySQL implements DriverInterface {
                 ':height'          => $image->getHeight(),
             ));
             $insertImageStatement->closeCursor();
-            
+
         } catch (\PDOException $e) {
             $pdo->rollback();
             throw new DatabaseException('Unable to save image data', 500, $e);
@@ -153,7 +153,7 @@ class MySQL implements DriverInterface {
     /**
      * @see PHPIMS\Database\DriverInterface::deleteImage()
      */
-    public function deleteImage($imageIdentifier) {
+    public function deleteImage($publicKey, $imageIdentifier) {
         try {
             $deleteImageStatement = $this->pdo->prepare("
                 DELETE FROM
@@ -161,11 +161,11 @@ class MySQL implements DriverInterface {
                 WHERE
                     imageIdentifier = :imageIdentifier
             ");
-            
+
             $deleteImageStatement->execute(array(
                 ':imageIdentifier' => $imageIdentifier,
             ));
-            
+
             $deleteImageStatement->closeCursor();
         } catch (\PDOException $e) {
             throw new DatabaseException('Unable to delete image data: ' . $e->getMessage(), 500, $e);
@@ -177,23 +177,23 @@ class MySQL implements DriverInterface {
     /**
      * @see PHPIMS\Database\DriverInterface::editMetadata()
      */
-    public function updateMetadata($imageIdentifier, array $metadata) {
+    public function updateMetadata($publicKey, $imageIdentifier, array $metadata) {
         try {
             $this->pdo->beginTransaction();
-            
+
             $this->deleteMetadata($imageIdentifier);
-            
+
             if ($metadata)
             {
                 $insertValues = array();
-                
+
                 foreach ($metadata as $field => $value)
                 {
                     $insertValues[] = $imageIdentifier;
                     $insertValues[] = $field;
                     $insertValues[] = $value;
                 }
-            
+
                 $insertMetadataStatement = $this->pdo->prepare("
                     INSERT INTO
                         image_metadata
@@ -201,12 +201,12 @@ class MySQL implements DriverInterface {
                     VALUES
                         " . self::getInsertValuesString(3, count($metadata)) . "
                 ");
-                
+
                 $insertMetadataStatement->execute($insertValues);
-                
+
                 $insertMetadataStatement->closeCursor();
             }
-            
+
             $this->pdo->commit();
         } catch (\PDOException $e) {
             throw new DatabaseException('Unable to edit image data: ' . $e->getMessage(), 500, $e);
@@ -218,14 +218,14 @@ class MySQL implements DriverInterface {
     /**
      * @see PHPIMS\Database\DriverInterface::getMetadata()
      */
-    public function getMetadata($imageIdentifier) {
+    public function getMetadata($publicKey, $imageIdentifier) {
         return $this->getMetadataForImages(array($imageIdentifier));
     }
 
     /**
      * @see PHPIMS\Database\DriverInterface::deleteMetadata()
      */
-    public function deleteMetadata($imageIdentifier) {
+    public function deleteMetadata($publicKey, $imageIdentifier) {
         try {
             $deleteImageMetadataStatement = $this->pdo->prepare("
                 DELETE FROM
@@ -233,11 +233,11 @@ class MySQL implements DriverInterface {
                 WHERE
                     imageIdentifier = :imageIdentifier
             ");
-            
+
             $deleteImageMetadataStatement->execute(array(
                 ':imageIdentifier' => $imageIdentifier,
             ));
-            
+
             $deleteImageMetadataStatement->closeCursor();
         } catch (\PDOException $e) {
             throw new DatabaseException('Unable to remove metadata', 500, $e);
@@ -249,38 +249,38 @@ class MySQL implements DriverInterface {
     /**
      * @see PHPIMS\Database\DriverInterface::getImages()
      */
-    public function getImages(Query $query) {
+    public function getImages($publicKey, Query $query) {
         $images = array();
 
         $additionalTables = array();
         $whereStatements = array();
         $whereValues = array();
-        
+
         if ($query->from())
         {
             $whereStatements[] = 'AND i.added > :timeFrom';
             $whereValues[':timeFrom'] = $query->from();
         }
-        
+
         if ($query->to())
         {
             $whereStatements[] = 'AND i.added < :timeTo';
             $whereValues[':timeTo'] = $query->to();
         }
-        
+
         if ($query->query()) {
             // we should probably have a better solution than self-joining for each metadata query statement
             $placeholderIndex = 0;
-            
+
             foreach($query->query() as $field => $value)
             {
                 $additionalTables[] = 'image_metadata im' . $placeholderIndex;
                 $whereStatements[] = 'AND i.imageIdentifier = im' . $placeholderIndex . '.imageIdentifier';
-            
+
                 $whereStatements[] = 'AND im' . $placeholderIndex . '.field = :metaField_' . $placeholderIndex . ' AND im' . $placeholderIndex . '.value = :metaValue_' . $placeholderIndex;
                 $whereValues[':metaField_' . $placeholderIndex] = $field;
                 $whereValues[':metaValue_' . $placeholderIndex] = $value;
-                
+
                 $placeholderIndex++;
             }
         }
@@ -288,17 +288,17 @@ class MySQL implements DriverInterface {
         try {
             $start = $query->num() * ($query->page() - 1);
             $generatedSQL = '';
-            
+
             if ($additionalTables)
             {
                 $generatedSQL .= ', ' . join(', ', $additionalTables);
             }
-            
+
             if ($whereStatements)
             {
                 $generatedSQL .= 'WHERE ' . join(' ', $whereStatements);
             }
-            
+
             $imagesStatement = $this->pdo->prepare("
                 SELECT
                     i.*
@@ -308,37 +308,37 @@ class MySQL implements DriverInterface {
                 LIMIT
                     :start, :num
             ");
-            
+
             // we have to bindValue each statement, as we can't provide PDO::PARAM_INT when using ->execute()
             $imagesStatement->bindValue(':start', $start, \PDO::PARAM_INT);
             $imagesStatement->bindValue(':num', (int) $query->num(), \PDO::PARAM_INT);
-            
+
             foreach($whereValues as $placeholder => $value)
             {
                 $imagesStatement->bindValue($placeholder, $value);
             }
-            
+
             $imagesStatement->execute();
-            
+
             $images = $imagesStatement->fetchAll(\PDO::FETCH_ASSOC);
-            
+
             if ($query->returnMetadata()) {
                 // build array with all images referenced by their imageidentifier
                 $imagesByIdentifier = array();
-                
+
                 foreach($images as $image)
                 {
                     $imagesByIdentifier[$image['imageIdentifier']] = $image;
                 }
-                
+
                 // returns metadata by imageidentifier
                 $metadataByIdentifier = $this->getMetadataForImages(array_keys($imagesByIdentifier));
-                
+
                 foreach($metadataByIdentifier as $identifier => $metadata)
                 {
                     $imagesByIdentifier[$identifier]['metadata'] = $metadata;
                 }
-                
+
                 // replace array with images array with added metadata
                 $images = array_values($imagesByIdentifier);
             }
@@ -352,7 +352,7 @@ class MySQL implements DriverInterface {
     /**
      * @see PHPIMS\Database\DriverInterface::load()
      */
-    public function load($imageIdentifier, Image $image) {
+    public function load($publicKey, $imageIdentifier, Image $image) {
         try {
             $loadImageStatement = $this->pdo->prepare("
                 SELECT
@@ -362,11 +362,11 @@ class MySQL implements DriverInterface {
                 WHERE
                     imageIdentifier = :imageIdentifier
             ");
-            
+
             $loadImageStatement->execute(array(
                 ':imageIdentifier' => $imageIdentifier,
             ));
-            
+
             $data = $loadImageStatement->fetch(\PDO::FETCH_ASSOC);
             $loadImageStatement->closeCursor();
         } catch (\PDOException $e) {
@@ -380,21 +380,21 @@ class MySQL implements DriverInterface {
 
         return true;
     }
-    
+
     /**
      * Get metadata for an array of image identifiers.
-     * 
+     *
      * @param array $imageIdentifiers A list of image identifiers to fetch data for.
      */
     protected function getMetadataForImages(array $imageIdentifiers)
     {
         $metadata = array();
-        
+
         foreach($imageIdentifiers as $imageIdentifier)
         {
             $metadata[$imageIdentifier] = array();
         }
-    
+
         try {
             $getMetadataStatement = $this->pdo->prepare("
                 SELECT
@@ -404,21 +404,21 @@ class MySQL implements DriverInterface {
                 WHERE
                     m.imageIdentifier IN " . self::getPlaceholderExpression(count($imageIdentifiers)) . "
             ");
-            
+
             $getMetadataStatement->execute($imageIdentifiers);
-            
+
             $rows = $getMetadataStatement->fetchAll(\PDO::FETCH_ASSOC);
-            
+
             foreach($rows as $row)
             {
                 $metadata[$row['imageIdentifier']][$row['field']] = $row['value'];
             }
-            
+
             $getMetadataStatement->closeCursor();
         } catch (\PDOException $e) {
             throw new DatabaseException('Unable to fetch image metadata: ' . $e->getMessage(), 500, $e);
         }
-        
+
         return $metadata;
     }
 
@@ -439,21 +439,21 @@ class MySQL implements DriverInterface {
                 WHERE
                     imageIdentifier = :imageIdentifier
             ");
-            
+
             $imageExistsStatement->execute(array(
                 ':imageIdentifier' => $imageIdentifier,
             ));
-            
+
             $rowTest = $imageExistsStatement->fetch();
-            
+
             $imageExistsStatement->closeCursor();
-            
+
             return !empty($rowTest);
         } catch (\PDOException $e) {
             throw new DatabaseException('Unable to test for existance of image: ' . $e->getMessage(), 500, $e);
         }
     }
-    
+
     /**
      * Helper method to generate a placeholder string for multi-row inserts.
      *
@@ -481,5 +481,5 @@ class MySQL implements DriverInterface {
         $eachValueStr = '(' . substr(str_repeat('?, ', $columns), 0, -2) . ')';
 
         return $eachValueStr;
-    }    
+    }
 }
