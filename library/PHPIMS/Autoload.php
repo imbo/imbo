fb20e3d614fc646b394313ddc83d8bec1fedd857
Christer Edvartsen <cogo@starzinger.net>
2011-07-28 12:36:13 +0200

    Removed MySQL driver. Will be added when @matslindh updates it.

--- library/PHPIMS/Autoload.php
+++ library/PHPIMS/Autoload.php
@@ -57,7 +57,6 @@ class Autoload {
         'phpims\\client\\imageurl' => '/Client/ImageUrl.php',
         'phpims\\client\\response' => '/Client/Response.php',
         'phpims\\database\\driver\\mongodb' => '/Database/Driver/MongoDB.php',
-        'phpims\\database\\driver\\mysql' => '/Database/Driver/MySQL.php',
         'phpims\\database\\driverinterface' => '/Database/DriverInterface.php',
         'phpims\\database\\exception' => '/Database/Exception.php',
         'phpims\\exception' => '/Exception.php',
