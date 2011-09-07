ba8a8dc3170d42b5425a827c9c557c3d6413d11b
Christer Edvartsen <cogo@starzinger.net>
2011-09-07 17:24:11 +0200

    Added test case for the IdentifyImage plugin

---
+++ tests/PHPIMS/Resource/Plugin/IdentifyImageTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * PHPIMS
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package PHPIMS
+ * @subpackage Unittests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+
+namespace PHPIMS\Resource\Plugin;
+
+/**
+ * @package PHPIMS
+ * @subpackage Unittests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/phpims
+ */
+class IdentifyImageTest extends \PHPUnit_Framework_TestCase {
+    private $plugin;
+    private $request;
+    private $response;
+    private $database;
+    private $storage;
+
+    public function setUp() {
+        $this->plugin   = new IdentifyImage();
+        $this->request  = $this->getMock('PHPIMS\Request\RequestInterface');
+        $this->response = $this->getMock('PHPIMS\Response\ResponseInterface');
+        $this->database = $this->getMock('PHPIMS\Database\DatabaseInterface');
+        $this->storage  = $this->getMock('PHPIMS\Storage\StorageInterface');
+    }
+
+    public function tearDown() {
+        $this->plugin = null;
+    }
+
+    /**
+     * @expectedException PHPIMS\Resource\Plugin\Exception
+     * @expectedExceptionMessage Unsupported image type
+     * @expectedExceptionCode 415
+     */
+    public function testExecWithUnsupportedImageType() {
+        $image = $this->getMock('PHPIMS\Image\ImageInterface');
+        $image->expects($this->once())->method('getBlob')->will($this->returnValue(file_get_contents(__FILE__)));
+        $this->response->expects($this->once())->method('getImage')->will($this->returnValue($image));
+
+        $this->plugin->exec($this->request, $this->response, $this->database, $this->storage);
+    }
+
+    public function testSuccessfulExec() {
+        // Path to a PNG image
+        $imagePath = __DIR__ . '/../../_files/image.png';
+
+        // Image identifier with wrong extension
+        $imageIdentifier = md5_file($imagePath) . '.jpg';
+        $correctImageIdentifier = md5_file($imagePath) . '.png';
+
+        $image = $this->getMock('PHPIMS\Image\ImageInterface');
+        $image->expects($this->once())->method('getBlob')->will($this->returnValue(file_get_contents($imagePath)));
+        $image->expects($this->once())->method('setMimeType')->with('image/png')->will($this->returnValue($image));
+        $image->expects($this->once())->method('setExtension')->with('png')->will($this->returnValue($image));
+
+        // Intentionally set a wrong extension to make sure the plugin will fix it for us
+        $this->request->expects($this->once())->method('getImageIdentifier')->will($this->returnValue($imageIdentifier));
+        $this->request->expects($this->once())->method('setImageIdentifier')->with($correctImageIdentifier);
+
+        $this->response->expects($this->once())->method('getImage')->will($this->returnValue($image));
+
+        $this->plugin->exec($this->request, $this->response, $this->database, $this->storage);
+    }
+}
