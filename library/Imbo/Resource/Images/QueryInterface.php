62951ef9ce6a7af0622b18efd0895f200e56022d
Christer Edvartsen <cogo@starzinger.net>
2011-10-07 10:58:59 +0200

    Added two interfaces

---
+++ library/Imbo/Resource/Images/QueryInterface.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Imbo
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package Imbo
+ * @subpackage Interfaces
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imbo
+ */
+
+namespace Imbo\Resource\Images;
+
+/**
+ * Query interface
+ *
+ * @package Imbo
+ * @subpackage Interfaces
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imbo
+ */
+interface QueryInterface {
+    /**
+     * Set or get the page property
+     *
+     * @param int $page Give this a value to set the page property
+     * @return int|Imbo\Resource\Images\QueryInterface
+     */
+    function page($page = null);
+
+    /**
+     * Set or get the num property
+     *
+     * @param int $num Give this a value to set the num property
+     * @return int|Imbo\Resource\Images\QueryInterface
+     */
+    function num($num = null);
+
+    /**
+     * Set or get the returnMetadata flag
+     *
+     * @param boolean $returnMetadata Give this a value to set the returnMetadata flag
+     * @return boolean|Imbo\Resource\Images\QueryInterface
+     */
+    function returnMetadata($returnMetadata = null);
+
+    /**
+     * Set or get the metadataQuery property
+     *
+     * @param array $metadataQuery Give this a value to set the property
+     * @return array|Imbo\Resource\Images\QueryInterface
+     */
+    function metadataQuery(array $metadataQuery = null);
+
+    /**
+     * Set or get the from attribute
+     *
+     * @param int $from Give this a value to set the from property
+     * @return int|Imbo\Resource\Images\QueryInterface
+     */
+    function from($from = null);
+
+    /**
+     * Set or get the to attribute
+     *
+     * @param int $from Give this a value to set the to property
+     * @return int|Imbo\Resource\Images\QueryInterface
+     */
+    function to($to = null);
+}
---
+++ library/Imbo/Resource/ImagesInterface.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Imbo
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package Imbo
+ * @subpackage Interfaces
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imbo
+ */
+
+namespace Imbo\Resource;
+
+use Imbo\Resource\Images\QueryInterface;
+
+/**
+ * Images resource interface
+ *
+ * This interface extends the resource interface with two methods for setting and getting a query
+ * instance.
+ *
+ * @package Imbo
+ * @subpackage Interfaces
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imbo
+ */
+interface ImagesInterface extends ResourceInterface {
+    /**
+     * Fetch a query instance
+     *
+     * @return Imbo\Resource\Images\QueryInterface
+     */
+    function getQuery();
+
+    /**
+     * Set a query instance
+     *
+     * @param Imbo\Resource\Images\QueryInterface $query A query instance
+     * @return Imbo\Resource\ImagesInterface
+     */
+    function setQuery(QueryInterface $query);
+}
