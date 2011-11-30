8d25fcd8a0ce6770eaf7e532b0da69aa4a0d27bf
Christer Edvartsen <cogo@starzinger.net>
2011-11-30 07:47:08 +0100

    Added valiator classes for the timestamp and the signature along with interfaces and tests

---
+++ library/Imbo/Validate/SignatureInterface.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Imbo
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package Core
+ * @subpackage Interfaces
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imbo
+ */
+
+namespace Imbo\Validate;
+
+/**
+ * Signature validator interface
+ *
+ * @package Core
+ * @subpackage Interfaces
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imbo
+ */
+interface SignatureInterface extends ValidateInterface {
+    /**
+     * Set the HTTP method to use when generating the signature
+     *
+     * @param string $method The method to set
+     * @return Imbo\Validate\SignatureInterface
+     */
+    function setHttpMethod($method);
+
+    /**
+     * Set the URL to use when generating the signature
+     *
+     * @param string $url The URL to set
+     * @return Imbo\Validate\SignatureInterface
+     */
+    function setUrl($url);
+
+    /**
+     * Set the timestamp to use when generating the signature
+     *
+     * @param string $timestamp The timestamp to set
+     * @return Imbo\Validate\SignatureInterface
+     */
+    function setTimestamp($timestamp);
+
+    /**
+     * Set the public key to use when generating the signature
+     *
+     * @param string $key The key to set
+     * @return Imbo\Validate\SignatureInterface
+     */
+    function setPublicKey($key);
+
+    /**
+     * Set the private key to use when generating the signature
+     *
+     * @param string $key The key to set
+     * @return Imbo\Validate\SignatureInterface
+     */
+    function setPrivateKey($key);
+}
