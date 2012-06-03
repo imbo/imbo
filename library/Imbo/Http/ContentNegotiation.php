7d2009f80cc01bd75f6aecc23e6514fe91d59140
Christer Edvartsen <cogo@starzinger.net>
2012-06-03 13:40:33 +0200

    Added a method that can be used to select the best match in a set of mime types matched against acceptable types

--- library/Imbo/Http/ContentNegotiation.php
+++ library/Imbo/Http/ContentNegotiation.php
@@ -42,6 +42,28 @@ namespace Imbo\Http;
  */
 class ContentNegotiation {
     /**
+     * Pick the best match from a set of mime types matched against acceptable types
+     *
+     * @param array $mimeTypes The mime types to find the best match from
+     * @param array $acceptable Acceptable types to match against
+     * @return boolean|string Returns false if none of the $mimetypes are acceptable or the best
+     *                        match on success
+     */
+    public function bestMatch(array $mimeTypes, array $acceptable) {
+        $maxQ = 0;
+        $match = false;
+
+        foreach ($mimeTypes as $mime) {
+            if (($q = $this->isAcceptable($mime, $acceptable)) && ($q > $maxQ)) {
+                $maxQ = $q;
+                $match = $mime;
+            }
+        }
+
+        return $match;
+    }
+
+    /**
      * See if a mime type is accepted
      *
      * @param string $mimeType The mime type to check, for instance "image/png"
