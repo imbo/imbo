5f35c19a3fbb66eb5d4c057acb92e4512671049b
Christer Edvartsen <cogo@starzinger.net>
2012-10-21 13:46:35 +0200

    Added an event listener that handles 304 Not Modified

---
+++ library/Imbo/EventListener/NotModified.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Imbo
+ *
+ * Copyright (c) 2011-2012, Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package EventListener
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011-2012, Christer Edvartsen <cogo@starzinger.net>
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/imbo/imbo
+ */
+
+namespace Imbo\EventListener;
+
+use Imbo\EventManager\EventInterface,
+    Imbo\Http\Request\RequestInterface,
+    Imbo\Exception\RuntimeException;
+
+/**
+ * @package EventListener
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011-2012, Christer Edvartsen <cogo@starzinger.net>
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/imbo/imbo
+ */
+class NotModified extends Listener implements ListenerInterface {
+    /**
+     * {@inheritdoc}
+     */
+    public function getEvents() {
+        return array(
+            'user.get.post',
+            'images.get.post',
+            'image.get.post',
+            'metadata.get.post',
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function invoke(EventInterface $event) {
+        $container = $event->getContainer();
+
+        $responseHeaders = $container->response->getHeaders();
+        $requestHeaders = $container->request->getHeaders();
+
+        $ifModifiedSince = $requestHeaders->get('if-modified-since');
+        $ifNoneMatch = $requestHeaders->get('if-none-match');
+        $lastModified = $responseHeaders->get('last-modified');
+        $etag = $responseHeaders->get('etag');
+
+        if (
+            $ifModifiedSince && $ifNoneMatch && (
+                $lastModified === $ifModifiedSince &&
+                $etag === $ifNoneMatch
+            )
+        ) {
+            $container->response->setNotModified();
+        }
+    }
+}
