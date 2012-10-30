28b54db02185bce3c24d9a5a0422afb5e1eb6b93
Espen Hovlandsdal <rexxars@gmail.com>
2012-10-30 11:09:36 +0100

    Added a newline before the if-statement.

--- library/Imbo/Http/Response/ResponseWriter.php
+++ library/Imbo/Http/Response/ResponseWriter.php
@@ -107,6 +107,7 @@ class ResponseWriter implements ResponseWriterInterface {
         if ($extension = $request->getExtension()) {
             // The user agent wants a specific type. Skip content negotiation completely
             $mime = $this->defaultMimeType;
+
             if (isset($this->extensionsToMimeType[$extension])) {
                 $mime = $this->extensionsToMimeType[$extension];
             }
