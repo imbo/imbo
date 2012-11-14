33a10721a80c7a29c6c61ad440aa2edfc51af4e4
Christer Edvartsen <cogo@starzinger.net>
2012-11-14 16:38:07 +0100

    Added a test case that makes sure the dist config has the corrent transformation names

---
+++ tests/Imbo/UnitTest/ConfigTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Imbo
+ *
+ * Copyright (c) 2011-2012, Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package TestSuite\UnitTests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011-2012, Christer Edvartsen <cogo@starzinger.net>
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/imbo/imbo
+ */
+
+namespace Imbo\UnitTest;
+
+/**
+ * @package TestSuite\UnitTests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011-2012, Christer Edvartsen <cogo@starzinger.net>
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/imbo/imbo
+ * @covers Imbo\FrontController
+ */
+class ConfigTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * Make sure the transformation names are correct
+     */
+    public function testTransformationNames() {
+        $config = require __DIR__ . '/../../../config/config.php.dist';
+
+        $this->assertArrayHasKey('transformations', $config);
+
+        $transformations = $config['transformations'];
+
+        $expected = array(
+            'border',
+            'canvas',
+            'compress',
+            'convert',
+            'crop',
+            'desaturate',
+            'flipHorizontally',
+            'flipVertically',
+            'maxSize',
+            'resize',
+            'rotate',
+            'thumbnail',
+            'transpose',
+            'transverse',
+        );
+
+        $this->assertSame(count($expected), count($transformations));
+
+        foreach ($expected as $transformation) {
+            $this->assertArrayHasKey($transformation, $transformations);
+        }
+    }
+}
