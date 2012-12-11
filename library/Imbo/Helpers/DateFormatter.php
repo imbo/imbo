c480d48c901e87cfa52debec7dd72d52609c690f
Christer Edvartsen <cogo@starzinger.net>
2012-12-11 13:20:07 +0100

    Force UTC on the DateTime instance

--- library/Imbo/Helpers/DateFormatter.php
+++ library/Imbo/Helpers/DateFormatter.php
@@ -31,7 +31,8 @@
 
 namespace Imbo\Helpers;
 
-use DateTime;
+use DateTime,
+    DateTimeZone;
 
 /**
  * Date formatter helper class
@@ -50,6 +51,7 @@ class DateFormatter {
      * @return string Returns a formatted date string
      */
     public function formatDate(DateTime $date) {
+        $date->setTimezone(new DateTimeZone('UTC'));
         return $date->format('D, d M Y H:i:s') . ' GMT';
     }
 }
