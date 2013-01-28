d0686700a0e070cb94b4e8dfe68931ac6513596e
Christer Edvartsen <cogo@starzinger.net>
2013-01-28 20:32:50 +0100

    Added setters and getters and added a test case

--- library/Imbo/Model/Error.php
+++ library/Imbo/Model/Error.php
@@ -24,33 +24,138 @@ class Error implements ModelInterface {
      *
      * @var int
      */
-    public $httpCode;
+    private $httpCode;
 
     /**
      * Error message from Imbo
      *
      * @var string
      */
-    public $errorMessage;
+    private $errorMessage;
 
     /**
      * Current date
      *
      * @var DateTime
      */
-    public $date;
+    private $date;
 
     /**
      * Internal Imbo error code
      *
      * @var int
      */
-    public $imboErrorCode;
+    private $imboErrorCode;
 
     /**
      * Optional image identifier
      *
      * @var string
      */
-    public $imageIdentifier;
+    private $imageIdentifier;
+
+    /**
+     * Set the HTTP code
+     *
+     * @param int $code The code to set
+     * @return Error
+     */
+    public function setHttpCode($code) {
+        $this->httpCode = (int) $code;
+
+        return $this;
+    }
+
+    /**
+     * Get the HTTP code
+     *
+     * @return int
+     */
+    public function getHttpCode() {
+        return $this->httpCode;
+    }
+
+    /**
+     * Set the error message
+     *
+     * @param string $message The message to set
+     * @return Error
+     */
+    public function setErrorMessage($message) {
+        $this->errorMessage = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get the error message
+     *
+     * @return string
+     */
+    public function getErrorMessage() {
+        return $this->errorMessage;
+    }
+
+    /**
+     * Set the date
+     *
+     * @param DateTime $date The DateTime instance to set
+     * @return Error
+     */
+    public function setDate(DateTime $date) {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get the date
+     *
+     * @return DateTime
+     */
+    public function getDate() {
+        return $this->date;
+    }
+
+    /**
+     * Set the imbo error code
+     *
+     * @param int $code The code to set
+     * @return Error
+     */
+    public function setImboErrorCode($code) {
+        $this->imboErrorCode = (int) $code;
+
+        return $this;
+    }
+
+    /**
+     * Get the imbo error code
+     *
+     * @return int
+     */
+    public function getImboErrorCode() {
+        return $this->imboErrorCode;
+    }
+
+    /**
+     * Set the image identifier
+     *
+     * @param string $imageIdentifier The image identifier to set
+     * @return Error
+     */
+    public function setImageIdentifier($imageIdentifier) {
+        $this->imageIdentifier = $imageIdentifier;
+
+        return $this;
+    }
+
+    /**
+     * Get the image identifier
+     *
+     * @return string
+     */
+    public function getImageIdentifier() {
+        return $this->imageIdentifier;
+    }
 }
---
+++ tests/Imbo/UnitTest/Model/ErrorTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace Imbo\UnitTest\Model;
+
+use Imbo\Model\Error,
+    DateTime;
+
+/**
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @package Test suite\Unit tests
+ * @covers Imbo\Model\Error
+ */
+class ErrorTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var Error
+     */
+    private $model;
+
+    /**
+     * Set up the model
+     */
+    public function setUp() {
+        $this->model = new Error();
+    }
+
+    /**
+     * Tear down the model
+     */
+    public function tearDown() {
+        $this->model = null;
+    }
+
+    public function testCanSetAndGetHttpCode() {
+        $this->assertNull($this->model->getHttpCode());
+        $this->assertSame($this->model, $this->model->setHttpCode(404));
+        $this->assertSame(404, $this->model->getHttpCode());
+    }
+
+    public function testCanSetAndGetErrorMessage() {
+        $this->assertNull($this->model->getErrorMessage());
+        $this->assertSame($this->model, $this->model->setErrorMessage('message'));
+        $this->assertSame('message', $this->model->getErrorMessage());
+    }
+
+    public function testCanSetAndGetDate() {
+        $date = new DateTime();
+        $this->assertNull($this->model->getDate());
+        $this->assertSame($this->model, $this->model->setDate($date));
+        $this->assertSame($date, $this->model->getDate());
+    }
+
+    public function testCanSetAndGetImboErrorCode() {
+        $this->assertNull($this->model->getImboErrorCode());
+        $this->assertSame($this->model, $this->model->setImboErrorCode(100));
+        $this->assertSame(100, $this->model->getImboErrorCode());
+    }
+
+    public function testCanSetAndGetImageIdentifier() {
+        $this->assertNull($this->model->getImageIdentifier());
+        $this->assertSame($this->model, $this->model->setImageIdentifier('identifier'));
+        $this->assertSame('identifier', $this->model->getImageIdentifier());
+    }
+}
