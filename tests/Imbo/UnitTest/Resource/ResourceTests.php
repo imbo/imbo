6e7df4878e3dcc731a9a7d5ceca6687be3a2875a
Christer Edvartsen <cogo@starzinger.net>
2013-02-06 07:58:30 +0100

    Added test case for allowed HTTP methods and event names

--- tests/Imbo/UnitTest/Resource/ResourceTests.php
+++ tests/Imbo/UnitTest/Resource/ResourceTests.php
@@ -33,4 +33,37 @@ abstract class ResourceTests extends \PHPUnit_Framework_TestCase {
             $this->assertInstanceOf('Imbo\EventListener\ListenerDefinition', $d);
         }
     }
+
+    public function testReturnsTheCorrectAllowedMethods() {
+        $resource = $this->getNewResource();
+        $methods = $resource->getAllowedMethods();
+        $definition = $resource->getDefinition();
+
+        foreach ($methods as $method) {
+            $expectedEventName = strtolower(substr(get_class($resource), strrpos(get_class($resource), '\\') + 1) . '.' . $method);
+
+            foreach ($definition as $d) {
+                $eventName = $d->getEventName();
+
+                if ($eventName === $expectedEventName) {
+                    continue 2;
+                }
+            }
+
+            $this->fail('Resource allows ' . $method . ', but no listener definition subscribes to ' . $expectedEventName);
+        }
+
+        foreach ($definition as $d) {
+            $eventName = $d->getEventName();
+            $expectedMethod = strtoupper(substr($eventName, strrpos($eventName, '.') + 1));
+
+            foreach ($methods as $method) {
+                if ($method === $expectedMethod) {
+                    continue 2;
+                }
+            }
+
+            $this->fail('Resource subscribes to ' . $eventName . ' but does not allow ' . $expectedMethod);
+        }
+    }
 }
