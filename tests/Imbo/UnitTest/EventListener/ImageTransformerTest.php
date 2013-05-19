c90550a806959c5d6b564d7e60c59f9ce5c74170
Espen Hovlandsdal <rexxars@gmail.com>
2013-05-19 16:41:36 +0200

    Inject an ImageReader into all applied ImageReaderAware transformations

--- tests/Imbo/UnitTest/EventListener/ImageTransformerTest.php
+++ tests/Imbo/UnitTest/EventListener/ImageTransformerTest.php
@@ -13,6 +13,8 @@ namespace Imbo\UnitTest\EventListener;
 use Imbo\EventListener\ImageTransformer,
     Imbo\Image\Transformation\Transformation,
     Imbo\Image\Transformation\TransformationInterface,
+    Imbo\Storage\ImageReaderAware,
+    Imbo\Storage\ImageReaderAwareTrait,
     Imbo\Model\Image;
 
 /**
@@ -27,9 +29,11 @@ class ImageTransformerTest extends ListenerTests {
 
     private $request;
     private $response;
+    private $storage;
     private $event;
     private $image;
     private $cn;
+    private $publicKey = 'pubKey';
 
     /**
      * Set up the listener
@@ -37,12 +41,15 @@ class ImageTransformerTest extends ListenerTests {
     public function setUp() {
         $this->cn = $this->getMock('Imbo\Http\ContentNegotiation');
         $this->request = $this->getMock('Imbo\Http\Request\Request');
+        $this->request->expects($this->any())->method('getPublicKey')->will($this->returnValue($this->publicKey));
         $this->image = $this->getMock('Imbo\Model\Image');
+        $this->storage = $this->getMock('Imbo\Storage\StorageInterface');
         $this->response = $this->getMock('Imbo\Http\Response\Response');
         $this->response->expects($this->any())->method('getImage')->will($this->returnValue($this->image));
         $this->event = $this->getMock('Imbo\EventManager\EventInterface');
         $this->event->expects($this->any())->method('getRequest')->will($this->returnValue($this->request));
         $this->event->expects($this->any())->method('getResponse')->will($this->returnValue($this->response));
+        $this->event->expects($this->any())->method('getStorage')->will($this->returnValue($this->storage));
 
         $this->listener = new ImageTransformer();
     }
@@ -116,6 +123,29 @@ class ImageTransformerTest extends ListenerTests {
 
         $this->listener->transform($this->event);
     }
+
+    /**
+     * @covers Imbo\EventListener\ImageTransformer::transform
+     * @covers Imbo\EventListener\ImageTransformer::registerTransformationHandler
+     */
+    public function testSetsImageReaderIfTransformationImplementsImageReaderAware() {
+        $this->request->expects($this->once())->method('getTransformations')->will($this->returnValue(array(
+            array(
+                'name' => 'thumbnail',
+                'params' => array(),
+            ),
+        )));
+
+        $this->storage->expects($this->once())->method('getImage')->will($this->returnValue($this->publicKey));
+
+        $this->listener->registerTransformationHandler('thumbnail', function($params) {
+            return new ImageReaderAwareTransformation('imagereaderaware');
+        });
+        $this->image->expects($this->once())->method('hasBeenTransformed')->with(true);
+
+        $this->expectOutputString('imagereaderaware-' . $this->publicKey);
+        $this->listener->transform($this->event);
+    }
 }
 
 /**
@@ -123,7 +153,7 @@ class ImageTransformerTest extends ListenerTests {
  * @package Test suite\Unit tests
  */
 class SomeTransformation extends Transformation implements TransformationInterface {
-    private $output;
+    protected $output;
 
     public function __construct($output) {
         $this->output = $output;
@@ -131,5 +161,17 @@ class SomeTransformation extends Transformation implements TransformationInterfa
 
     public function applyToImage(Image $image) {
         echo $this->output;
+    }
+}
+
+/**
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @package Test suite\Unit tests
+ */
+class ImageReaderAwareTransformation extends SomeTransformation implements ImageReaderAware, TransformationInterface {
+    use ImageReaderAwareTrait;
+
+    public function applyToImage(Image $image) {
+        echo $this->output . '-' . $this->getImageReader()->getImage('someImg');
     }
 }
