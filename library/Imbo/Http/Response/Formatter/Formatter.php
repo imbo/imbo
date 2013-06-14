a7021428dfb29ddad72cb66a8434b9a6861d0320
Christer Edvartsen <cogo@starzinger.net>
2013-06-14 10:44:48 +0200

    Added support for the list model

--- library/Imbo/Http/Response/Formatter/Formatter.php
+++ library/Imbo/Http/Response/Formatter/Formatter.php
@@ -57,6 +57,8 @@ abstract class Formatter implements FormatterInterface {
             return $this->formatMetadata($model);
         } else if ($model instanceof Model\ArrayModel) {
             return $this->formatArrayModel($model);
+        } else if ($model instanceof Model\ListModel) {
+            return $this->formatListModel($model);
         }
 
         throw new InvalidArgumentException('Unsupported model type', 500);
