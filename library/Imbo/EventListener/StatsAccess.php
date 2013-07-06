8a21cfad9079c94be68ce018850ad35af4674643
Christer Edvartsen <cogo@starzinger.net>
2013-07-06 13:25:57 +0200

    Added support for IPv6

--- library/Imbo/EventListener/StatsAccess.php
+++ library/Imbo/EventListener/StatsAccess.php
@@ -49,6 +49,10 @@ class StatsAccess implements ListenerInterface {
     public function __construct(array $params = array()) {
         if ($params) {
             $this->params = array_replace_recursive($this->params, $params);
+
+            // Exand all IPv6 addresses in the filters
+            array_walk($this->params['whitelist'], array($this, 'expandIPv6InFilters'));
+            array_walk($this->params['blacklist'], array($this, 'expandIPv6InFilters'));
         }
     }
 
@@ -120,13 +124,28 @@ class StatsAccess implements ListenerInterface {
     }
 
     /**
-     * Check if an IPv4 is in a subnet
+     * Check if an IP address is in a subnet
+     *
+     * @param string $ip The IP address to check
+     * @param string $range A CIDR notated IP address and routing prefix
+     * @return boolean
+     */
+    private function cidrMatch($ip, $range) {
+        if ($this->isIPv6($ip)) {
+            return $this->cidr6Match($ip, $range);
+        } else {
+            return $this->cidr4Match($ip, $range);
+        }
+    }
+
+    /**
+     * Check an IPv4 address is in a subnet
      *
      * @param string $ip The IP address to check (for instance 192.168.1.10)
      * @param string $range A CIDR notated IP address and routing prefix (for instance 192.168.1.0/24)
      * @return boolean
      */
-    private function cidrMatch($ip, $range) {
+    private function cidr4Match($ip, $range) {
         // Split CIDR on /
         list($subnet, $bits) = explode('/', $range);
 
@@ -140,5 +159,98 @@ class StatsAccess implements ListenerInterface {
 
         // Check for match
         return ($ip & $mask) === $subnet;
+    }
+
+    /**
+     * Check an IPv6 address is in a subnet
+     *
+     * @param string $ip The IP address to check (for instance 2001:db8:0:0:0:0:0:0)
+     * @param string $range A CIDR notated IP address and routing prefix (for instance 2001:db8::/48)
+     * @return boolean
+     */
+    private function cidr6Match($ip, $range) {
+        // Split CIDR on /
+        list($subnet, $mask) = explode('/', $range);
+
+        // Convert IP's to their in_addr representations
+        $ip = inet_pton($ip);
+        $subnet = inet_pton($subnet);
+
+        // Convert the mask
+        $mask = $this->getBinaryMask((int) $mask);
+
+        return ($ip & $mask) === $subnet;
+    }
+
+    /**
+     * Fetch the binary representation of a mask
+     *
+     * @param int $mask A mask from a CIDR
+     * @return string
+     */
+    private function getBinaryMask($mask) {
+        // Prefix the string
+        $hexMask = str_repeat('f', $mask / 4);
+
+        // Add the remainder
+        switch ($mask % 4) {
+            case 1:
+                $hexMask .= '8';
+                break;
+            case 2:
+                $hexMask .= 'c';
+                break;
+            case 3:
+                $hexMask .= 'e';
+                break;
+        }
+
+        // Pad with 0 to 32 in length
+        $hexMask = str_pad($hexMask, 32, '0');
+
+        // Pack into binary string
+        return pack('H*' , $hexMask);
+    }
+
+    /**
+     * Check if an IP address is an IPv6 address or not
+     *
+     * @param string $ip The address to check
+     * @return boolean True if the ip address looks like an IPv6 address
+     */
+    private function isIPv6($ip) {
+        return strpos($ip, ':') !== false;
+    }
+
+    /**
+     * Expand a short IPv6
+     *
+     * @param string $ip For instance 2a00:1b60:1011::1338
+     * @return string 2a00:1b60:1011:0000:0000:0000:0000:1338
+     */
+    private function expandIPv6($ip) {
+        // Convert to in_addr an unpack as hex
+        $hex = strtolower(unpack('H*ip', inet_pton($ip))['ip']);
+
+        // Inject colons
+        return substr(preg_replace('/([a-f0-9]{4})/', '$1:', $hex), 0, -1);
+    }
+
+    /**
+     * Expand all IPv6 addresses in a filter
+     *
+     * @param string $ip An IP address that might be expanded
+     */
+    private function expandIPv6InFilters(&$ip) {
+        if ($this->isIPv6($ip)) {
+            if (($pos = strpos($ip, '/')) !== false) {
+                // The IPv6 has a routing prefix attached to it
+                $mask = substr($ip, $pos);
+                $ip = $this->expandIPv6(substr($ip, 0, $pos)) . $mask;
+            } else {
+                // Regular IPv6 address
+                $ip = $this->expandIPv6($ip);
+            }
+        }
     }
 }
