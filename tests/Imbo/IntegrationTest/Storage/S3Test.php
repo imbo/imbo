dd9a67a3002c0c2e69b728c003c03d9d9986885e
Christer Edvartsen <cogo@starzinger.net>
2013-07-12 09:00:54 +0200

    Added integration test case

---
+++ tests/Imbo/IntegrationTest/Storage/S3Test.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace Imbo\IntegrationTest\Storage;
+
+use Imbo\Storage\S3;
+
+/**
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @package Test suite\Integration tests
+ */
+class S3Test extends StorageTests {
+    /**
+     * @see Imbo\IntegrationTest\Storage\StorageTests::getDriver()
+     */
+    protected function getDriver() {
+        return new S3(array(
+            'key' => $GLOBALS['AWS_S3_KEY'],
+            'secret' => $GLOBALS['AWS_S3_SECRET'],
+            'bucket' => $GLOBALS['AWS_S3_BUCKET'],
+        ));
+    }
+
+    /**
+     * Make sure we have the correct config available
+     */
+    public function setUp() {
+        foreach (array('AWS_S3_KEY', 'AWS_S3_SECRET', 'AWS_S3_BUCKET') as $key) {
+            if (empty($GLOBALS[$key])) {
+                $this->markTestSkipped('This test needs the ' . $key . ' value to be set in phpunit.xml');
+            }
+        }
+
+        parent::setUp();
+    }
+}
