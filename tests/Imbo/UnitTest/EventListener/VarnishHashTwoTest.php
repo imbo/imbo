8b0ebd0431dd3ec24f51f370e7591ce900fd88d2
Christer Edvartsen <cogo@starzinger.net>
2013-09-19 12:24:42 +0200

    Added an event listener that sends a HashTwo header that can be used by Varnish

---
+++ tests/Imbo/UnitTest/EventListener/VarnishHashTwoTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace Imbo\UnitTest\EventListener;
+
+use Imbo\EventListener\VarnishHashTwo;
+
+/**
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @package Test suite\Unit tests
+ */
+class VarnishHashTwoTest extends ListenerTests {
+    /**
+     * @var VarnishHashTwo
+     */
+    private $listener;
+
+    private $event;
+    private $request;
+    private $response;
+    private $responseHeaders;
+
+    /**
+     * Set up the listener
+     */
+    public function setUp() {
+        $this->request = $this->getMock('Imbo\Http\Request\Request');
+
+        $this->responseHeaders = $this->getMock('Symfony\Component\HttpFoundation\ResponseHeaderBag');
+        $this->response = $this->getMock('Imbo\Http\Response\Response');
+        $this->response->headers = $this->responseHeaders;
+
+        $this->event = $this->getMock('Imbo\EventManager\EventInterface');
+        $this->event->expects($this->any())->method('getRequest')->will($this->returnValue($this->request));
+        $this->event->expects($this->any())->method('getResponse')->will($this->returnValue($this->response));
+
+        $this->listener = new VarnishHashTwo();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getListener() {
+        return $this->listener;
+    }
+
+    /**
+     * Tear down the listener
+     */
+    public function tearDown() {
+        $this->request = null;
+        $this->response = null;
+        $this->responseHeaders = null;
+        $this->event = null;
+        $this->listener = null;
+    }
+
+    /**
+     * @covers Imbo\EventListener\VarnishHashTwo::addHeader
+     */
+    public function testCanSendAHashTwoHeader() {
+        $this->request->expects($this->once())->method('getPublicKey')->will($this->returnValue('key'));
+        $image = $this->getMock('Imbo\Model\Image');
+        $image->expects($this->once())->method('getImageIdentifier')->will($this->returnValue('id'));
+        $this->response->expects($this->once())->method('getImage')->will($this->returnValue($image));
+        $this->responseHeaders->expects($this->once())->method('set')->with('X-HashTwo', 'key|id');
+
+        $this->listener->addHeader($this->event);
+    }
+}
