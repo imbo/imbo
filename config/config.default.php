9fcfefb6fee6ea94ec29cd238568a5f06ae185e5
Christer Edvartsen <cogo@starzinger.net>
2013-09-25 12:53:19 +0200

    Updated the event listener inline docs, and changed the default event listener config to use the new format

--- config/config.default.php
+++ config/config.default.php
@@ -67,24 +67,39 @@ $config = array(
      * for anything, but exists so you can override/unset some helpers from config.php). The values
      * of each element in this array can be one of the following:
      *
-     * 1) An instance of the Imbo\EventListener\ListenerInterface interface
+     * 1) A string representing a class name of a class implementing the
+     *    Imbo\EventListener\ListenerInteface interface
      *
-     * 2) A closure returning an instance of the Imbo\EventListener\ListenerInterface interface
+     * 2) An instance of an object implementing the Imbo\EventListener\ListenerInterface interface
      *
-     * 3) An array with the following keys:
+     * 3) A closure returning an instance of an object implementing the
+     *    Imbo\EventListener\ListenerInterface interface
      *
-     *   - listener
+     * 4) An array with the following keys:
+     *
+     *   - listener (required)
+     *   - params
      *   - publicKeys
      *
-     *   where 'listener' is an instance of the Imbo\EventListener\ListenerInterface interface or a
-     *   closure returning an instance of said interface, and 'publicKeys' is an array with one of
-     *   the following keys:
+     *   where 'listener' is one of the following:
      *
-     *     - include
-     *     - exclude
+     *     1) a string representing a class name of a class implementing the
+     *        Imbo\EventListener\ListenerInterface interface
      *
-     *     where 'include' is an array of public keys that the listener *will* trigger for, and
-     *     'exclude' is an array of public keys that the listener *will not* trigger for.
+     *     2) an instance of the Imbo\EventListener\ListenerInterface interface
+     *
+     *     3) a closure returning an instance Imbo\EventListener\ListenerInterface
+     *
+     *   'params' is an array with parameters for the constructor of the event listener. This is
+     *   only used when the 'listener' key is a string containing a class name.
+     *
+     *   'publicKeys' is an array with one of the following keys:
+     *
+     *     - whitelist
+     *     - blacklist
+     *
+     *     where 'whitelist' is an array of public keys that the listener *will* trigger for, and
+     *     'blacklist' is an array of public keys that the listener *will not* trigger for.
      *
      * 4) An array with the following keys:
      *
@@ -93,78 +108,75 @@ $config = array(
      *   - priority
      *   - publicKeys
      *
-     *   where 'events' is an array of events that 'callback' will subscribe to. If you have a
-     *   single callback that subscribes to several events, and you want to use different priorities
-     *   for the events, simply specify an associative array where the keys are the event names, and
-     *   the values are the priorities. If you use this method, the 'priority' key will be ignored.
+     *   where 'events' is an array of events that 'callback' will subscribe to. If your callback
+     *   subscribes to several events, and you want to use different priorities for the events,
+     *   simply specify an associative array where the keys are the event names, and the values are
+     *   the priorities for each event. If you use this method, the 'priority' key will be ignored.
      *
      *   'callback' is any callable function. The function will receive a single argument, which is
      *   an instance of Imbo\EventManager\EventInterface.
      *
-     *   'priority' is the priority of your callback. This defaults to 1 (low priority). The
+     *   'priority' is the priority of your callback. This defaults to 0 (low priority). The
      *   priority can also be a negative number if you want your listeners to be triggered after
      *   Imbo's event listeners.
      *
      *   'publicKeys' is the same as described above.
      *
-     * Example of how to add listeners:
+     * Examples of how to add listeners:
      *
      * 'eventListeners' => array(
-     *   // 1) Implementation of a listener interface
+     *   // 1) A class name in a string
+     *   'accessToken' => 'Imbo\EventListener\ListenerInterface',
+     *
+     *   // 2) Implementation of a listener interface
      *   'auth' => new EventListener\Authenticate(),
      *
-     *   // 2) Implementation of a listener interface with a public key filter
+     *   // 3) Implementation of a listener interface with a public key filter
      *   'maxImageSize' => array(
      *     'listener' => EventListener\MaxImageSize(1024, 768),
      *     'publicKeys' => array(
-     *       'include' => array( ... ),
-     *       // 'exclude' => array( ... ),
+     *       'whitelist' => array( ... ),
+     *       // 'blacklist' => array( ... ),
      *       )
      *     )
      *   ),
      *
-     *   // 3) A closure that will subscribe to two events using one priority
-     *   'someCustomCallback' => array(
-     *     'callback' => function($event) {
-     *       // Some code
-     *     },
-     *     'events' => array(
-     *       'image.get', 'image.post'
-     *     ),
-     *     'priority' => 20,
-     *     'publicKeys' => array(
-     *       'include' => array( ... ),
-     *       // 'exclude' => array( ... ),
-     *     )
+     *   // 4) A class name in a string with custom parameters for the listener
+     *   'statsAccess' => array(
+     *       'listener' => 'Imbo\EventListener\StatsAccess',
+     *       'params' => array(
+     *           array(
+     *               'whitelist' => array('127.0.0.1', '::1'),
+     *               'blacklist' => array(),
+     *           )
+     *       ),
      *   ),
      *
-     *   // 4) A closure that will subscribe to two events with different priorities
+     *   // 5) A closure that will subscribe to two events with different priorities
      *   'anotherCustomCallback' => array(
-     *     'callback' => function($event) {
-     *       // Some code
-     *     },
-     *     'events' => array(
-     *       'image.get' => 20, // Trigger BEFORE the internal handler for "image.get"
-     *       'image.post' => -20, // Trigger AFTER the internal handler for "image.post"
-     *     ),
+     *       'callback' => function($event) {
+     *           // Some code
+     *       },
+     *       'events' => array(
+     *           'image.get' => 20, // Trigger BEFORE the internal handler for "image.get"
+     *           'image.post' => -20, // Trigger AFTER the internal handler for "image.post"
+     *       ),
      *   ),
-     * ),
      *
      * @var array
      */
     'eventListeners' => array(
-        'accessToken' => function() {
-            return new EventListener\AccessToken();
-        },
-        'auth' => function() {
-            return new EventListener\Authenticate();
-        },
-        'statsAccess' => function() {
-            return new EventListener\StatsAccess(array(
-                'whitelist' => array('127.0.0.1', '::1'),
-                'blacklist' => array(),
-            ));
-        },
+        'accessToken' => 'Imbo\EventListener\AccessToken',
+        'auth' => 'Imbo\EventListener\Authenticate',
+        'statsAccess' => array(
+            'listener' => 'Imbo\EventListener\StatsAccess',
+            'params' => array(
+                array(
+                    'whitelist' => array('127.0.0.1', '::1'),
+                    'blacklist' => array(),
+                )
+            ),
+        ),
     ),
 
     /**
