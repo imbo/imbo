54e062b85111b39347ac8f9ef46dc987cbfcb86f
Christer Edvartsen <cogo@starzinger.net>
2013-10-15 16:43:27 +0200

    Moved some of the transformation handling responsibility to the image model. Also updated the unit test for the image transformer

--- library/Imbo/EventListener/ImageTransformer.php
+++ library/Imbo/EventListener/ImageTransformer.php
@@ -12,7 +12,6 @@ namespace Imbo\EventListener;
 
 use Imbo\EventManager\EventInterface,
     Imbo\Exception\TransformationException,
-    Imbo\Storage\ImageReaderAware,
     Imbo\Storage\ImageReader,
     Imbo\Image\Transformation\TransformationInterface,
     Imbo\Model\Image;
@@ -25,29 +24,14 @@ use Imbo\EventManager\EventInterface,
  */
 class ImageTransformer implements ListenerInterface {
     /**
-     * An array of registered transformation handlers
-     *
-     * @var array
-     */
-    private $transformationHandlers = array();
-
-    /**
-     * An image reader instance
-     *
-     * @var ImageReader
-     */
-    private $imageReader;
-
-    /**
      * {@inheritdoc}
      */
     public static function getSubscribedEvents() {
         return array(
-            // Initialize the image transformations from the configuration
-            'image.get' => array('initialize' => 1),
-
-            // Transform the image
-            'image.transform' => 'transform',
+            'image.transform' => array(
+                'transform' => 0,
+                'initialize' => 10,
+            ),
         );
     }
 
@@ -57,8 +41,14 @@ class ImageTransformer implements ListenerInterface {
      * @param EventInterface $event The current event
      */
     public function initialize(EventInterface $event) {
-        foreach ($event->getConfig()['imageTransformations'] as $name => $callback) {
-            $this->registerTransformationHandler($name, $callback);
+        $request = $event->getRequest();
+        $imageReader = new ImageReader($request->getPublicKey(), $event->getStorage());
+
+        $image = $event->getResponse()->getModel();
+        $image->setImageReader($imageReader);
+
+        foreach ($event->getConfig()['imageTransformations'] as $name => $transformation) {
+            $image->setTransformationHandler($name, $transformation);
         }
     }
 
@@ -72,69 +62,12 @@ class ImageTransformer implements ListenerInterface {
         $image = $event->getResponse()->getModel();
         $transformed = false;
 
-        // Fetch and apply transformations
+        // Fetch transformations specifed in the query and transform the image
         foreach ($request->getTransformations() as $transformation) {
-            $name = $transformation['name'];
-            $params = $transformation['params'];
-            $transformation = null;
-
-            if (!isset($this->transformationHandlers[$name])) {
-                throw new TransformationException('Unknown transformation: ' . $name, 400);
-            }
-
-            $callback = $this->transformationHandlers[$name];
-
-            if (is_string($callback)) {
-                $transformation = new $callback($params);
-            } else if (is_callable($callback)) {
-                $transformation = $callback($params);
-            }
-
-            if (!($transformation instanceof TransformationInterface)) {
-                throw new TransformationException('Invalid image transformation: ' . $name, 500);
-            }
-
-            if ($transformation instanceof ImageReaderAware) {
-                $transformation->setImageReader($this->getImageReader($event));
-            }
-
-            $transformation->applyToImage($image);
+            $image->transform($transformation['name'], $transformation['params']);
             $transformed = true;
         }
 
         $image->hasBeenTransformed($transformed);
-    }
-
-    /**
-     * Register an image transformation handler
-     *
-     * @param string $name The name of the transformation, as used in the query parameters
-     * @param callable $callback A piece of code that can be executed. The callback will receive a
-     *                           single parameter: $params, which is an array with parameters
-     *                           associated with the transformation. The callable must return an
-     *                           instance of Imbo\Image\Transformation\TransformationInterface
-     * @return ImageTransformer
-     */
-    public function registerTransformationHandler($name, $callback) {
-        $this->transformationHandlers[$name] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Fetch an instance of an image reader
-     *
-     * @param  EventInterface $event The event that triggered the transformation
-     * @return ImageReader An image reader instance
-     */
-    private function getImageReader(EventInterface $event) {
-        if (!$this->imageReader) {
-            $publicKey = $event->getRequest()->getPublicKey();
-            $storage = $event->getStorage();
-
-            $this->imageReader = new ImageReader($publicKey, $storage);
-        }
-
-        return $this->imageReader;
     }
 }
--- tests/Imbo/UnitTest/EventListener/ImageTransformerTest.php
+++ tests/Imbo/UnitTest/EventListener/ImageTransformerTest.php
@@ -20,6 +20,7 @@ use Imbo\EventListener\ImageTransformer,
 /**
  * @author Christer Edvartsen <cogo@starzinger.net>
  * @package Test suite\Unit tests
+ * @covers Imbo\EventListener\ImageTransformer
  */
 class ImageTransformerTest extends ListenerTests {
     /**
@@ -29,27 +30,20 @@ class ImageTransformerTest extends ListenerTests {
 
     private $request;
     private $response;
-    private $storage;
     private $event;
     private $image;
-    private $cn;
-    private $publicKey = 'pubKey';
 
     /**
      * Set up the listener
      */
     public function setUp() {
-        $this->cn = $this->getMock('Imbo\Http\ContentNegotiation');
         $this->request = $this->getMock('Imbo\Http\Request\Request');
-        $this->request->expects($this->any())->method('getPublicKey')->will($this->returnValue($this->publicKey));
         $this->image = $this->getMock('Imbo\Model\Image');
-        $this->storage = $this->getMock('Imbo\Storage\StorageInterface');
         $this->response = $this->getMock('Imbo\Http\Response\Response');
         $this->response->expects($this->any())->method('getModel')->will($this->returnValue($this->image));
         $this->event = $this->getMock('Imbo\EventManager\Event');
         $this->event->expects($this->any())->method('getRequest')->will($this->returnValue($this->request));
         $this->event->expects($this->any())->method('getResponse')->will($this->returnValue($this->response));
-        $this->event->expects($this->any())->method('getStorage')->will($this->returnValue($this->storage));
 
         $this->listener = new ImageTransformer();
     }
@@ -63,7 +57,6 @@ class ImageTransformerTest extends ListenerTests {
         $this->image = null;
         $this->event = null;
         $this->listener = null;
-        $this->cn = null;
     }
 
     /**
@@ -74,8 +67,32 @@ class ImageTransformerTest extends ListenerTests {
     }
 
     /**
+     * @covers Imbo\EventListener\ImageTransformer::initialize
+     */
+    public function testWillInitializeImageTransformationHandlers() {
+        $config = array(
+            'imageTransformations' => array(
+                'border' => 'Imbo\Image\Transformation\Border',
+                'thumbnail' => 'Imbo\Image\Transformation\Thumbnail',
+                'borderedThumbnail' => array(
+                    'border', 'thumbnail',
+                ),
+
+            ),
+        );
+        $this->request->expects($this->once())->method('getPublicKey')->will($this->returnValue('publickey'));
+        $this->event->expects($this->once())->method('getStorage')->will($this->returnValue($this->getMock('Imbo\Storage\StorageInterface')));
+        $this->image->expects($this->at(0))->method('setImageReader')->with($this->isInstanceOf('Imbo\Storage\ImageReader'));
+        $this->event->expects($this->once())->method('getConfig')->will($this->returnValue($config));
+        $this->image->expects($this->at(1))->method('setTransformationHandler')->with('border', 'Imbo\Image\Transformation\Border');
+        $this->image->expects($this->at(2))->method('setTransformationHandler')->with('thumbnail', 'Imbo\Image\Transformation\Thumbnail');
+        $this->image->expects($this->at(3))->method('setTransformationHandler')->with('borderedThumbnail', array('border', 'thumbnail'));
+
+        $this->listener->initialize($this->event);
+    }
+
+    /**
      * @covers Imbo\EventListener\ImageTransformer::transform
-     * @covers Imbo\EventListener\ImageTransformer::registerTransformationHandler
      */
     public function testCanApplyTransformationsToAnImage() {
         $this->request->expects($this->once())->method('getTransformations')->will($this->returnValue(array(
@@ -93,109 +110,19 @@ class ImageTransformerTest extends ListenerTests {
             ),
         )));
 
-        $this->listener->registerTransformationHandler('resize', function($params) {
-            return new SomeTransformation($params);
-        });
-        $this->listener->registerTransformationHandler('thumbnail', __NAMESPACE__ . '\SomeOtherTransformation');
-
-        $this->image->expects($this->once())->method('hasBeenTransformed')->with(true);
-
-        $this->expectOutputString('a:1:{s:5:"width";i:100;}a:1:{s:4:"some";s:5:"value";}');
-        $this->listener->transform($this->event);
-    }
-
-    /**
-     * @expectedException Imbo\Exception\TransformationException
-     * @expectedExceptionMessage Unknown transformation: foo
-     * @expectedExceptionCode 400
-     * @covers Imbo\EventListener\ImageTransformer::transform
-     */
-    public function testThrowsExceptionWhenApplyingUnknownTransformation() {
-        $this->request->expects($this->once())->method('getTransformations')->will($this->returnValue(array(
-            array(
-                'name' => 'foo',
-                'params' => array(
-                    'width' => 100,
-                ),
-            )
-        )));
+        $this->image->expects($this->at(0))->method('transform')->with('resize', array('width' => 100));
+        $this->image->expects($this->at(1))->method('transform')->with('thumbnail', array('some' => 'value'));
+        $this->image->expects($this->at(2))->method('hasBeenTransformed')->with(true);
 
         $this->listener->transform($this->event);
     }
 
     /**
      * @covers Imbo\EventListener\ImageTransformer::transform
-     * @covers Imbo\EventListener\ImageTransformer::registerTransformationHandler
      */
-    public function testSetsImageReaderIfTransformationImplementsImageReaderAware() {
-        $this->request->expects($this->once())->method('getTransformations')->will($this->returnValue(array(
-            array(
-                'name' => 'thumbnail',
-                'params' => array(),
-            ),
-        )));
-
-        $this->storage->expects($this->once())->method('getImage')->will($this->returnValue($this->publicKey));
-
-        $this->listener->registerTransformationHandler('thumbnail', function($params) {
-            return new ImageReaderAwareTransformation('imagereaderaware');
-        });
-        $this->image->expects($this->once())->method('hasBeenTransformed')->with(true);
-
-        $this->expectOutputString($this->publicKey);
+    public function testDoesNotMarkImageAsTransformedIfNoTransformationsExists() {
+        $this->request->expects($this->once())->method('getTransformations')->will($this->returnValue(array()));
+        $this->image->expects($this->at(0))->method('hasBeenTransformed')->with(false);
         $this->listener->transform($this->event);
-    }
-
-    /**
-     * @expectedException Imbo\Exception\TransformationException
-     * @expectedExceptionMessage Invalid image transformation: transformation
-     * @expectedExceptionCode 500
-     * @covers Imbo\EventListener\ImageTransformer::transform
-     */
-    public function testCallbacksAreNotAllowedAsImageTransformations() {
-        $this->request->expects($this->once())->method('getTransformations')->will($this->returnValue(array(
-            array(
-                'name' => 'transformation',
-                'params' => array(),
-            ),
-        )));
-        $this->listener->registerTransformationHandler('transformation', function ($params) {
-            return function ($image) {};
-        });
-        $this->listener->transform($this->event);
-    }
-}
-
-/**
- * @author Christer Edvartsen <cogo@starzinger.net>
- * @package Test suite\Unit tests
- */
-class SomeTransformation extends Transformation implements TransformationInterface {
-    protected $params;
-
-    public function __construct($params) {
-        $this->params = $params;
-    }
-
-    public function applyToImage(Image $image) {
-        echo serialize($this->params);
-    }
-}
-
-/**
- * @author Christer Edvartsen <cogo@starzinger.net>
- * @package Test suite\Unit tests
- */
-class SomeOtherTransformation extends SomeTransformation {}
-
-/**
- * @author Christer Edvartsen <cogo@starzinger.net>
- * @package Test suite\Unit tests
- */
-class ImageReaderAwareTransformation extends SomeTransformation implements ImageReaderAware, TransformationInterface {
-    use ImageReaderAwareTrait;
-
-    public function applyToImage(Image $image) {
-        echo $this->getImageReader()->getImage('someImg');
     }
 }
