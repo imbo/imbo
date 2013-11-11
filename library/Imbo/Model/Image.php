1cb23c9810e0c26a531c51ccbeede093df5da59c
Christer Edvartsen <cogo@starzinger.net>
2013-11-11 21:24:33 +0100

    Removed image transformation code as transformations have been changed to event listeners

--- library/Imbo/Model/Image.php
+++ library/Imbo/Model/Image.php
@@ -10,11 +10,7 @@
 
 namespace Imbo\Model;
 
-use Imbo\Exception\TransformationException,
-    Imbo\Storage\ImageReader,
-    Imbo\Storage\ImageReaderAware,
-    Imbo\Image\Transformation\TransformationInterface,
-    DateTime;
+use DateTime;
 
 /**
  * Image model
@@ -91,13 +87,6 @@ class Image implements ModelInterface {
     private $checksum;
 
     /**
-     * Flag used with image transformations
-     *
-     * @var boolean
-     */
-    private $transformed = false;
-
-    /**
      * Added date
      *
      * @var DateTime
@@ -124,20 +113,6 @@ class Image implements ModelInterface {
      * @var string
      */
     private $imageIdentifier;
-
-    /**
-     * Image transformation handlers
-     *
-     * @var array
-     */
-    private $transformationHandlers = array();
-
-    /**
-     * Image reader that will be passed to optional transformations
-     *
-     * @var ImageReader
-     */
-    private $imageReader;
 
     /**
      * Get the size of the image data in bytes
@@ -394,64 +369,6 @@ class Image implements ModelInterface {
     }
 
     /**
-     * Update or get the transformed flag
-     *
-     * @param boolean $flag Set this to true or false to update the current flag. If not specified
-     *                      this method will return the current value of this flag.
-     * @return Image|boolean
-     */
-    public function hasBeenTransformed($flag = null) {
-        if ($flag === null) {
-            return $this->transformed;
-        }
-
-        $this->transformed = (boolean) $flag;
-
-        return $this;
-    }
-
-    /**
-     * Register an image transformation handler
-     *
-     * @param string $name The name of the transformation, as used in the query parameters
-     * @param mixed $transformation A class name, an array, a transforamation implementation or a
-     *                              callable piece of code that, when executed, returns an instance
-     *                              of an image transformation.
-     * @return self
-     */
-    public function setTransformationHandler($name, $transformation) {
-        $this->transformationHandlers[$name] = $transformation;
-
-        return $this;
-    }
-
-    /**
-     * Apply a transformation to the image
-     *
-     * @param string $name The name of the transformation handler, for instance "border"
-     * @param array $params Parameters for the transformation handler
-     * @return self
-     */
-    public function transform($name, array $params = array()) {
-        $transformations = $this->getTransformationHandler($name, $params);
-
-        foreach ($transformations as $transformation) {
-            $transformationParams = $transformation['params'];
-            $transformation = $transformation['transformation'];
-
-            if ($transformation instanceof ImageReaderAware) {
-                $transformation->setImageReader($this->imageReader);
-            }
-
-            $transformation->applyToImage($this, $transformationParams);
-        }
-
-        $this->hasBeenTransformed(true);
-
-        return $this;
-    }
-
-    /**
      * Check if a mime type is supported by Imbo
      *
      * @param string $mime The mime type to check. For instance "image/png"
@@ -459,18 +376,6 @@ class Image implements ModelInterface {
      */
     static public function supportedMimeType($mime) {
         return isset(self::$mimeTypes[$mime]);
-    }
-
-    /**
-     * Set an image reader
-     *
-     * @param ImageReader $reader An instance of an image reader
-     * @return self
-     */
-    public function setImageReader(ImageReader $reader) {
-        $this->imageReader = $reader;
-
-        return $this;
     }
 
     /**
@@ -482,60 +387,5 @@ class Image implements ModelInterface {
      */
     static public function getFileExtension($mime) {
         return isset(self::$mimeTypes[$mime]) ? self::$mimeTypes[$mime] : false;
-    }
-
-    /**
-     * Fetch an instance of a transformation handler
-     *
-     * @param string $name The name of the handler
-     * @param array $params Parameters for the handler
-     * @return TransformationInterface[] Returns an array of transformation handlers, usually with
-     *                                   only one element. The only time the array has more than
-     *                                   one is when we are dealing with presets that contains
-     *                                   multiple transformations
-     */
-    private function getTransformationHandler($name, array $params = array()) {
-        if (!isset($this->transformationHandlers[$name])) {
-            throw new TransformationException('Unknown transformation: ' . $name, 400);
-        }
-
-        // Initialize return value
-        $transformations = array();
-        $transformationHandler = $this->transformationHandlers[$name];
-
-        if (is_string($transformationHandler)) {
-            // The transformation is a class name
-            $transformations[] = array(
-                'transformation' => new $transformationHandler(),
-                'params' => $params,
-            );
-        } else if (is_callable($transformationHandler)) {
-            // We have a closure
-            $transformations[] = array(
-                'transformation' => $transformationHandler(),
-                'params' => $params,
-            );
-        } else if (is_array($transformationHandler)) {
-            // Preset with multiple transformations
-            foreach ($transformationHandler as $transformation => $p) {
-                if (is_int($transformation)) {
-                    $transformation = $p;
-                    $p = $params;
-                }
-
-                $transformations = array_merge(
-                    $transformations,
-                    $this->getTransformationHandler($transformation, $p)
-                );
-            }
-        }
-
-        foreach ($transformations as $transformation) {
-            if (!($transformation['transformation'] instanceof TransformationInterface)) {
-                throw new TransformationException('Invalid image transformation: ' . $name, 500);
-            }
-        }
-
-        return $transformations;
     }
 }
