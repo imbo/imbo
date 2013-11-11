f4ab9c88b9cabf1cf66cfd0a35c6fc75f1d72cf4
Christer Edvartsen <cogo@starzinger.net>
2013-11-11 17:22:13 +0100

    Added a custom PriorityQueue class that uses a more predictable way of prioritizing elements in the list with the same priority

---
+++ library/Imbo/EventManager/PriorityQueue.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace Imbo\EventManager;
+
+use SplPriorityQueue;
+
+/**
+ * A priority queue used for event listeners
+ *
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @package Event\Manager
+ */
+class PriorityQueue extends SplPriorityQueue {
+    /**
+     * Queue order counter
+     *
+     * @var int
+     */
+    private $queueOrder = PHP_INT_MAX;
+
+    /**
+     * Add an entry to the queue
+     *
+     * @param mixed $datum The entry to add
+     * @param int $priority The priority of the entry in the queue
+     */
+    public function insert($datum, $priority) {
+        if (is_int($priority)) {
+            $priority = array($priority, $this->queueOrder--);
+        }
+
+        parent::insert($datum, $priority);
+    }
+}
