409a640b8e5ddaa1f35750be0631d1e831645047
Christer Edvartsen <cogo@starzinger.net>
2013-12-03 15:26:48 +0100

    Added the Progressive transformation and a test case

---
+++ library/Imbo/Image/Transformation/Progressive.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace Imbo\Image\Transformation;
+
+use Imbo\Exception\TransformationException,
+    Imbo\EventListener\ListenerInterface,
+    Imbo\EventManager\EventInterface,
+    Imagick,
+    ImagickException;
+
+/**
+ * Progressive image transformation
+ *
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @package Image\Transformations
+ */
+class Progressive extends Transformation implements ListenerInterface {
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents() {
+        return array(
+            'image.transformation.progressive' => 'transform',
+        );
+    }
+
+    /**
+     * Transform the image
+     *
+     * @param EventInterface $event The event instance
+     */
+    public function transform(EventInterface $event) {
+        try {
+            $this->imagick->setInterlaceScheme(Imagick::INTERLACE_PLANE);
+            $event->getArgument('image')->hasBeenTransformed(true);
+        } catch (ImagickException $e) {
+            throw new TransformationException($e->getMessage(), 400, $e);
+        }
+    }
+}
