85c696d8d805c0c210b506d2c9a6df4736635bce
Christer Edvartsen <cogo@starzinger.net>
2013-12-06 17:48:38 +0100

    Don't add extension=imagick.so to php.ini as the pecl install command seems to do this by itself. Also dump some more debug info regarding Imagick

--- imagicktest.php
+++ imagicktest.php
@@ -3,4 +3,4 @@ $imagick = new Imagick();
 $imagick->readImageBlob(file_get_contents('tests/Imbo/Fixtures/640x160_rotated.jpg'));
 
 echo "getImageOrientation() => " . $imagick->getImageOrientation() . PHP_EOL;
-echo "getImageProperties()['exif:Orientation'] => " . $imagick->getImageProperties()['exif:Orientation'] . PHP_EOL;
+var_dump($imagick->getImageProperties());
