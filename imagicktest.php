e7ac0314441ee88f134df22ee09c46661200d71e
Christer Edvartsen <cogo@starzinger.net>
2013-12-07 14:30:35 +0100

    Remove .png test

--- imagicktest.php
+++ imagicktest.php
@@ -6,7 +6,6 @@ $imagick->readImageBlob(file_get_contents($filename));
 echo "getImageOrientation() => " . $imagick->getImageOrientation() . PHP_EOL;
 var_dump($imagick->getImageProperties());
 var_dump(exif_read_data($filename, 'EXIF'));
-var_dump(exif_read_data('tests/Imbo/Fixtures/image.png', 'EXIF'));
 
 $imagick = new Imagick();
 $imagick->readImageBlob(file_get_contents('tests/Imbo/Fixtures/exif-logo.jpg'));
