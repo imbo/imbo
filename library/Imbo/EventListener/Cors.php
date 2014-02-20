d1b3a12d5a96c0eec5534fd5bf9306d2fa1952fd
Christer Edvartsen <cogo@starzinger.net>
2014-02-20 16:58:34 +0100

    Updated the CORS listener with some new default values

--- library/Imbo/EventListener/Cors.php
+++ library/Imbo/EventListener/Cors.php
@@ -31,14 +31,16 @@ class Cors implements ListenerInterface {
     private $params = array(
         'allowedOrigins' => array(),
         'allowedMethods' => array(
-            'index' => array('GET', 'HEAD'),
-            'image' => array('GET', 'HEAD'),
-            'images' => array('GET', 'HEAD'),
-            'metadata' => array('GET', 'HEAD'),
-            'status' => array('GET', 'HEAD'),
-            'stats' => array('GET', 'HEAD'),
-            'user' => array('GET', 'HEAD'),
+            'index'          => array('GET', 'HEAD'),
+            'image'          => array('GET', 'HEAD'),
+            'images'         => array('GET', 'HEAD'),
+            'metadata'       => array('GET', 'HEAD'),
+            'status'         => array('GET', 'HEAD'),
+            'stats'          => array('GET', 'HEAD'),
+            'user'           => array('GET', 'HEAD'),
             'globalshorturl' => array('GET', 'HEAD'),
+            'shorturl'       => array('GET', 'HEAD'),
+            'shorturls'      => array('GET', 'HEAD'),
         ),
         'maxAge' => 3600,
     );
