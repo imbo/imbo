5f966774151afd2bf4e83f8eba484e799950a25a
Espen Hovlandsdal <rexxars@gmail.com>
2014-12-09 10:22:44 +0100

    Updated database schemas

--- setup/doctrine.mysql.sql
+++ setup/doctrine.mysql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `imageinfo` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `publicKey` varchar(255) COLLATE utf8_danish_ci NOT NULL,
+    `user` varchar(255) COLLATE utf8_danish_ci NOT NULL,
     `imageIdentifier` char(32) COLLATE utf8_danish_ci NOT NULL,
     `size` int(10) unsigned NOT NULL,
     `extension` varchar(5) COLLATE utf8_danish_ci NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `imageinfo` (
     `checksum` char(32) COLLATE utf8_danish_ci NOT NULL,
     `originalChecksum` char(32) COLLATE utf8_danish_ci NOT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `image` (`publicKey`,`imageIdentifier`)
+    UNIQUE KEY `image` (`user`,`imageIdentifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `metadata` (
@@ -26,35 +26,35 @@ CREATE TABLE IF NOT EXISTS `metadata` (
 
 CREATE TABLE IF NOT EXISTS `shorturl` (
     `shortUrlId` char(7) COLLATE utf8_danish_ci NOT NULL,
-    `publicKey` varchar(255) COLLATE utf8_danish_ci NOT NULL,
+    `user` varchar(255) COLLATE utf8_danish_ci NOT NULL,
     `imageIdentifier` char(32) COLLATE utf8_danish_ci NOT NULL,
     `extension` char(3) COLLATE utf8_danish_ci DEFAULT NULL,
     `query` text COLLATE utf8_danish_ci NOT NULL,
     PRIMARY KEY (`shortUrlId`),
-    KEY `params` (`publicKey`,`imageIdentifier`,`extension`,`query`(255))
+    KEY `params` (`user`,`imageIdentifier`,`extension`,`query`(255))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 CREATE TABLE IF NOT EXISTS `storage_images` (
-    `publicKey` varchar(255) COLLATE utf8_danish_ci NOT NULL,
+    `user` varchar(255) COLLATE utf8_danish_ci NOT NULL,
     `imageIdentifier` char(32) COLLATE utf8_danish_ci NOT NULL,
     `data` blob NOT NULL,
     `updated` int(10) unsigned NOT NULL,
-    PRIMARY KEY (`publicKey`,`imageIdentifier`)
+    PRIMARY KEY (`user`,`imageIdentifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 CREATE TABLE IF NOT EXISTS `storage_image_variations` (
-    `publicKey` varchar(255) COLLATE utf8_danish_ci NOT NULL,
+    `user` varchar(255) COLLATE utf8_danish_ci NOT NULL,
     `imageIdentifier` char(32) COLLATE utf8_danish_ci NOT NULL,
     `width` int(10) unsigned NOT NULL,
     `data` blob NOT NULL,
-    PRIMARY KEY (`publicKey`,`imageIdentifier`,`width`)
+    PRIMARY KEY (`user`,`imageIdentifier`,`width`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 CREATE TABLE IF NOT EXISTS `imagevariations` (
-    `publicKey` varchar(255) COLLATE utf8_danish_ci NOT NULL,
+    `user` varchar(255) COLLATE utf8_danish_ci NOT NULL,
     `imageIdentifier` char(32) COLLATE utf8_danish_ci NOT NULL,
     `width` int(10) unsigned NOT NULL,
     `height` int(10) unsigned NOT NULL,
     `added` int(10) unsigned NOT NULL,
-    PRIMARY KEY (`publicKey`,`imageIdentifier`,`width`)
+    PRIMARY KEY (`user`,`imageIdentifier`,`width`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
