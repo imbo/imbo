d4a20e4b78b25d354da6052adf52cb5419fcf32e
Espen Hovlandsdal <rexxars@gmail.com>
2015-02-24 17:06:08 +0100

    Introduced a work-in-progress MongoDB access control adapter

---
+++ library/Imbo/Auth/AccessControl/Adapter/MongoDB.php
@@ -0,0 +1,348 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace Imbo\Auth\AccessControl\Adapter;
+
+use Imbo\Exception\InvalidArgumentException,
+    Imbo\Auth\AccessControl\UserQuery,
+    Imbo\Auth\AccessControl\GroupQuery,
+    MongoClient,
+    MongoCollection,
+    MongoException;
+
+/**
+ * MongoDB access control adapter
+ *
+ * Valid parameters for this driver:
+ *
+ * - (string) databaseName Name of the database. Defaults to 'imbo'
+ * - (string) server The server string to use when connecting to MongoDB. Defaults to
+ *                   'mongodb://localhost:27017'
+ * - (array) options Options to use when creating the MongoClient instance. Defaults to
+ *                   ['connect' => true, 'connectTimeoutMS' => 1000].
+ *
+ * @author Espen Hovlandsdal <espen@hovlandsdal.com>
+ * @package Core\Auth\AccessControl\Adapter
+ */
+class MongoDB extends AbstractAdapter implements MutableAdapterInterface {
+    /**
+     * Mongo client instance
+     *
+     * @var MongoClient
+     */
+    private $mongoClient;
+
+    /**
+     * The access control collection
+     *
+     * @var MongoCollection
+     */
+    private $aclCollection;
+
+    /**
+     * The access control group collection
+     *
+     * @var MongoCollection
+     */
+    private $aclGroupCollection;
+
+    /**
+     * Cached list of public keys details, keyed by public key
+     *
+     * @var array
+     */
+    private $publicKeys = [];
+
+    /**
+     * Cached list of resource groups, keyed by group name
+     *
+     * @var array
+     */
+    private $resourceGroups = [];
+
+    /**
+     * Parameters for the driver
+     *
+     * @var array
+     */
+    private $params = [
+        // Database name
+        'databaseName' => 'imbo',
+
+        // Server string and ctor options
+        'server'  => 'mongodb://localhost:27017',
+        'options' => ['connect' => true, 'connectTimeoutMS' => 1000],
+    ];
+
+    /**
+     * Class constructor
+     *
+     * @param array $params Parameters for the driver
+     * @param MongoClient $client MongoClient instance
+     * @param MongoCollection $collection MongoCollection instance for the image variation collection
+     */
+    public function __construct(array $params = null, MongoClient $client = null, MongoCollection $collection = null) {
+        if ($params !== null) {
+            $this->params = array_replace_recursive($this->params, $params);
+        }
+
+        if ($client !== null) {
+            $this->mongoClient = $client;
+        }
+
+        if ($collection !== null) {
+            $this->collection = $collection;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasAccess($publicKey, $resource, $user = null) {
+        $accessList = $this->getAccessListForPublicKey($publicKey);
+
+        foreach ($accessList as $acl) {
+            // If a user is specified, ensure the public key has access to the user
+            $userAccess = !$user || isset($acl['users']) && in_array($user, $acl['users']);
+            if (!$userAccess) {
+                continue;
+            }
+
+            // Figure out which resources the public key has access to, based on group or
+            // explicit definition
+            $resources = isset($acl['resources']) ? $acl['resources'] : [];
+            $group = isset($acl['group']) ? $acl['group'] : false;
+
+            // If the group specified has not been defined, throw an exception to help the user
+            if ($group && !isset($this->groups[$group])) {
+                throw new InvalidArgumentException('Group "' . $group . '" is not defined');
+            }
+
+            // If a group is specified, fetch resources belonging to that group
+            if ($group) {
+                $resources = $this->groups[$group];
+            }
+
+            if (in_array($resource, $resources)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUsers(UserQuery $query = null) {
+        if ($query === null) {
+            $query = new UserQuery();
+        }
+
+        return array_slice($this->users, $query->offset() ?: 0, $query->limit());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function userExists($user) {
+        return in_array($user, $this->users);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGroups(GroupQuery $query = null) {
+        $cursor = $this->getGroupsCollection()
+            ->find()
+            ->skip($query->offset() ?: 0)
+            ->limit($query->limit());
+
+        $groups = [];
+        foreach ($cursor as $group) {
+            $groups[$group['name']] = $group['resources'];
+        }
+
+        return $groups;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGroup($groupName) {
+        $group = $this->getGroupsCollection()->findOne([
+            'name' => $groupName
+        ]);
+
+        if (isset($group['resources'])) {
+            return $group['resources'];
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPrivateKey($publicKey) {
+        $info = $this->getPublicKeyDetails($publicKey);
+
+        if (!$info || !isset($info['privateKey'])) {
+            return null;
+        }
+
+        return $info['privateKey'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addKeyPair($publicKey, $privateKey) {
+        throw new \Exception('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deletePublicKey($publicKey) {
+        throw new \Exception('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addAccessRule($publicKey, array $accessRule) {
+        throw new \Exception('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteAccessRule($publicKey, $accessId) {
+        throw new \Exception('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addResourceGroup($groupName, array $resources = []) {
+        throw new \Exception('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addResourcesToGroup($groupName, array $resources) {
+        throw new \Exception('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteResourceGroup($groupName) {
+        throw new \Exception('NOT IMPLEMENTED YET');
+    }
+
+    /**
+     * Get the access control list for a given public key
+     *
+     * @param  string $publicKey
+     * @return array
+     */
+    private function getAccessListForPublicKey($publicKey) {
+        $info = $this->getPublicKeyDetails($publicKey);
+
+        if (!$info || !isset($info['acl'])) {
+            return null;
+        }
+
+        return $info['acl'];
+    }
+
+    /**
+     * Get details for a given public key
+     *
+     * @param  string $publicKey
+     * @return array
+     */
+    private function getPublicKeyDetails($publicKey) {
+        if (isset($this->publicKeys[$publicKey])) {
+            return $this->publicKeys[$publicKey];
+        }
+
+        // Not in cache, fetch from database
+        $pubkeyInfo = $this->getAclCollection()->findOne([
+            'publicKey' => $publicKey
+        ]);
+
+        if ($pubkeyInfo) {
+            $this->publicKeys[$publicKey] = $pubkeyInfo;
+        }
+
+        return $pubkeyInfo;
+    }
+
+    /**
+     * Get the ACL mongo collection
+     *
+     * @return MongoCollection
+     */
+    private function getAclCollection() {
+        if ($this->aclCollection === null) {
+            try {
+                $this->aclCollection = $this->getMongoClient()->selectCollection(
+                    $this->params['databaseName'],
+                    'accesscontrol'
+                );
+            } catch (MongoException $e) {
+                throw new DatabaseException('Could not select collection', 500, $e);
+            }
+        }
+
+        return $this->aclCollection;
+    }
+
+    /**
+     * Get the resource groups mongo collection
+     *
+     * @return MongoCollection
+     */
+    private function getGroupsCollection() {
+        if ($this->aclGroupCollection === null) {
+            try {
+                $this->aclGroupCollection = $this->getMongoClient()->selectCollection(
+                    $this->params['databaseName'],
+                    'accesscontrolgroups'
+                );
+            } catch (MongoException $e) {
+                throw new DatabaseException('Could not select collection', 500, $e);
+            }
+        }
+
+        return $this->aclGroupCollection;
+    }
+
+    /**
+     * Get the mongo client instance
+     *
+     * @return MongoClient
+     */
+    private function getMongoClient() {
+        if ($this->mongoClient === null) {
+            try {
+                $this->mongoClient = new MongoClient($this->params['server'], $this->params['options']);
+            } catch (MongoException $e) {
+                throw new DatabaseException('Could not connect to database', 500, $e);
+            }
+        }
+
+        return $this->mongoClient;
+    }
+}
---
+++ tests/behat/features/access-control-keys.feature
@@ -0,0 +1,73 @@
+Feature: Imbo provides a keys endpoint
+    In order to query public keys
+    As an HTTP Client
+    I want to make requests against the keys endpoint
+
+    Background:
+        Given Imbo uses the "access-control-mutable.php" configuration
+
+    Scenario Outline: Fetch access control rules for a public key
+        Given I use "master-pubkey" and "master-privkey" for public and private keys
+        And I include an access token in the query
+        When I request "/keys/master-pubkey.<extension>/access"
+        Then I should get a response with "200 OK"
+        And the "Content-Type" response header is "<content-type>"
+        And the response body matches:
+        """
+        <response>
+        """
+        Examples:
+            | extension | content-type     | response |
+            | json      | application/json | #^\[{"id":".*?","resources":\["access\.get","access\.head"],"users":\[]},{"id":".*?","group":"something","users":\["some-user"]}]$# |
+            | xml       | application/xml  | #^<\?xml version="1\.0" encoding="UTF-8"\?>\s*<imbo>\s*<id>.*?</id>\s*<access>\s*<resources>\s*<resource>access\.get</resource>\s*<resource>access\.head</resource>\s*</resources>\s*</access>\s*<users\s*/>\s*</imbo>$#ms |
+
+        # I know, the XML here is broken. Find a proper format for it, plz?
+
+    Scenario: Create a public key
+        Given Imbo uses the "access-control-mutable.php" configuration
+        And I use "master-pubkey" and "master-privkey" for public and private keys
+        And the request body contains:
+          """
+          {"privateKey":"the-private-key"}
+          """
+        And I sign the request
+        When I request "/keys/foobar" using HTTP "PUT"
+        Then I should get a response with "201 Created"
+
+    Scenario: Update the private key for an existing public key
+        Given Imbo uses the "access-control-mutable.php" configuration
+        And I use "master-pubkey" and "master-privkey" for public and private keys
+        And the request body contains:
+          """
+          {"privateKey":"new-private-key"}
+          """
+        And I sign the request
+        When I request "/keys/foobar" using HTTP "PUT"
+        Then I should get a response with "200 OK"
+        # Perhaps add a test to ensure new keys can't be added/updated without the pubkey used having access to the keys endpoint?
+
+    Scenario: Update list of access rules for a public key
+        Given Imbo uses the "access-control-mutable.php" configuration
+        And I use "master-pubkey" and "master-privkey" for public and private keys
+        And the request body contains:
+          """
+          [{"resources":["images.get"],"users":["user1"]},{"group":"read-images","users":["user1", "user5"]}]
+          """
+        And I sign the request
+        When I request "/keys/foobar/access" using HTTP "POST"
+        Then I should get a response with "200 OK"
+
+    Scenario: Delete an access-control rule
+        Given Imbo uses the "access-control-mutable.php" configuration
+        And I use "master-pubkey" and "master-privkey" for public and private keys
+        And I sign the request
+        When I request "/keys/foobar/access/someId" using HTTP "DELETE"
+        Then I should get a response with "200 OK"
+        # How do we get the ID of a rule we can test? Prime the database?
+
+  Scenario: Delete a public key
+        Given Imbo uses the "access-control-mutable.php" configuration
+        And I use "master-pubkey" and "master-privkey" for public and private keys
+        And I sign the request
+        When I request "/keys/foobar" using HTTP "DELETE"
+        Then I should get a response with "200 OK"
