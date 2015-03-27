05e044a3cdeca20a9c9af703d7388447be9eca5a
Espen Hovlandsdal <rexxars@gmail.com>
2015-03-27 16:47:01 +0100

    Started work on updating tests to not use specific image identifiers

--- tests/behat/imbo-configs/config.testing.php
+++ tests/behat/imbo-configs/config.testing.php
@@ -8,23 +8,34 @@
  * distributed with this source code.
  */
 
+use Imbo\Auth\AccessControl\Adapter\AdapterInterface as ACI,
+    Imbo\Auth\AccessControl\Adapter\ArrayAdapter;
+
 // Default config for testing
 $testConfig = array(
-    'auth' => array(
-        'publickey' => 'privatekey',
-        'user' => 'key',
-    ),
+    'accessControl' => function() {
+        return new ArrayAdapter([
+            [
+                'publicKey' => 'publickey',
+                'privateKey' => 'privatekey',
+                'acl' => [[
+                    'resources' => ArrayAdapter::getReadWriteResources(),
+                    'users' => ['user'],
+                ]]
+            ],
+        ]);
+    },
 
     'database' => function() {
-        return new Imbo\Database\MongoDB(array(
+        return new Imbo\Database\MongoDB([
             'databaseName' => 'imbo_testing',
-        ));
+        ]);
     },
 
     'storage' => function() {
-        return new Imbo\Storage\GridFS(array(
+        return new Imbo\Storage\GridFS([
             'databaseName' => 'imbo_testing',
-        ));
+        ]);
     },
 );
 
@@ -35,7 +46,7 @@ $defaultConfig = require __DIR__ . '/../../../config/config.default.php';
 if (isset($_SERVER['HTTP_X_IMBO_TEST_CONFIG'])) {
     $customConfig = require __DIR__ . '/' . basename($_SERVER['HTTP_X_IMBO_TEST_CONFIG']);
 } else {
-    $customConfig = array();
+    $customConfig = [];
 }
 
 // Return the merged configuration, having the custom config overwrite the default testing config,
