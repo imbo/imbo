0b8fc4dd5d81ca90f0bdbd25b548ddafcfcc3256
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-10-06 16:26:58 +0200

    Add missing constants to access control resource list

--- library/Imbo/Auth/AccessControl/Adapter/AbstractAdapter.php
+++ library/Imbo/Auth/AccessControl/Adapter/AbstractAdapter.php
@@ -42,14 +42,35 @@ abstract class AbstractAdapter implements AdapterInterface {
      */
     final public static function getReadOnlyResources() {
         return [
-            ACI::RESOURCE_USER_GET,     ACI::RESOURCE_USER_HEAD,     ACI::RESOURCE_USER_OPTIONS,
-            ACI::RESOURCE_IMAGE_GET,    ACI::RESOURCE_IMAGE_HEAD,    ACI::RESOURCE_IMAGE_OPTIONS,
-            ACI::RESOURCE_GROUPS_GET,   ACI::RESOURCE_GROUPS_HEAD,   ACI::RESOURCE_GROUPS_OPTIONS,
-            ACI::RESOURCE_IMAGES_GET,   ACI::RESOURCE_IMAGES_HEAD,   ACI::RESOURCE_IMAGES_OPTIONS,
-            ACI::RESOURCE_METADATA_GET, ACI::RESOURCE_METADATA_HEAD, ACI::RESOURCE_METADATA_OPTIONS,
-            ACI::RESOURCE_SHORTURL_GET, ACI::RESOURCE_SHORTURL_HEAD, ACI::RESOURCE_SHORTURL_OPTIONS,
-            ACI::RESOURCE_GLOBAL_IMAGES_GET, ACI::RESOURCE_GLOBAL_IMAGES_HEAD,
-            ACI::RESOURCE_GLOBAL_IMAGES_OPTIONS, ACI::RESOURCE_SHORTURLS_OPTIONS
+            ACI::RESOURCE_USER_GET,
+            ACI::RESOURCE_USER_HEAD,
+            ACI::RESOURCE_USER_OPTIONS,
+
+            ACI::RESOURCE_IMAGE_GET,
+            ACI::RESOURCE_IMAGE_HEAD,
+            ACI::RESOURCE_IMAGE_OPTIONS,
+
+            ACI::RESOURCE_GROUPS_GET,
+            ACI::RESOURCE_GROUPS_HEAD,
+            ACI::RESOURCE_GROUPS_OPTIONS,
+
+            ACI::RESOURCE_IMAGES_GET,
+            ACI::RESOURCE_IMAGES_HEAD,
+            ACI::RESOURCE_IMAGES_OPTIONS,
+
+            ACI::RESOURCE_METADATA_GET,
+            ACI::RESOURCE_METADATA_HEAD,
+            ACI::RESOURCE_METADATA_OPTIONS,
+
+            ACI::RESOURCE_SHORTURL_GET,
+            ACI::RESOURCE_SHORTURL_HEAD,
+            ACI::RESOURCE_SHORTURL_OPTIONS,
+
+            ACI::RESOURCE_GLOBAL_IMAGES_GET,
+            ACI::RESOURCE_GLOBAL_IMAGES_HEAD,
+            ACI::RESOURCE_GLOBAL_IMAGES_OPTIONS,
+
+            ACI::RESOURCE_SHORTURLS_OPTIONS,
         ];
     }
 
@@ -61,10 +82,14 @@ abstract class AbstractAdapter implements AdapterInterface {
             self::getReadOnlyResources(), [
                 ACI::RESOURCE_IMAGE_DELETE,
                 ACI::RESOURCE_IMAGES_POST,
+
                 ACI::RESOURCE_METADATA_POST,
                 ACI::RESOURCE_METADATA_DELETE,
                 ACI::RESOURCE_METADATA_PUT,
+
                 ACI::RESOURCE_SHORTURL_DELETE,
+
+                ACI::RESOURCE_SHORTURLS_POST,
                 ACI::RESOURCE_SHORTURLS_DELETE,
             ]
         );
@@ -76,11 +101,19 @@ abstract class AbstractAdapter implements AdapterInterface {
     final public static function getAllResources() {
         return array_merge(
             self::getReadWriteResources(), [
-                ACI::RESOURCE_KEYS_PUT, ACI::RESOURCE_KEYS_DELETE, ACI::RESOURCE_KEYS_OPTIONS,
-                ACI::RESOURCE_ACCESS_RULE_GET, ACI::RESOURCE_ACCESS_RULE_HEAD,
-                ACI::RESOURCE_ACCESS_RULE_DELETE, ACI::RESOURCE_ACCESS_RULE_OPTIONS,
-                ACI::RESOURCE_ACCESS_RULES_GET, ACI::RESOURCE_ACCESS_RULES_HEAD,
-                ACI::RESOURCE_ACCESS_RULES_POST, ACI::RESOURCE_ACCESS_RULES_OPTIONS,
+                ACI::RESOURCE_KEYS_PUT,
+                ACI::RESOURCE_KEYS_DELETE,
+                ACI::RESOURCE_KEYS_OPTIONS,
+
+                ACI::RESOURCE_ACCESS_RULE_GET,
+                ACI::RESOURCE_ACCESS_RULE_HEAD,
+                ACI::RESOURCE_ACCESS_RULE_DELETE,
+                ACI::RESOURCE_ACCESS_RULE_OPTIONS,
+
+                ACI::RESOURCE_ACCESS_RULES_GET,
+                ACI::RESOURCE_ACCESS_RULES_HEAD,
+                ACI::RESOURCE_ACCESS_RULES_POST,
+                ACI::RESOURCE_ACCESS_RULES_OPTIONS,
             ]
         );
     }
