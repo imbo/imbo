18de8e0765a622e510440a143a7605663df627ab
Mats Lindh <mats@lindh.no>
2016-11-15 23:36:03 +0100

    Add test of required S3 parameters

---
+++ tests/phpunit/unit/Storage/S3Test.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace ImboUnitTest\Storage;
+
+use Imbo\Exception\ConfigurationException,
+    Imbo\Storage\S3;
+
+/**
+ * @covers Imbo\Storage\S3
+ * @group unit
+ * @group storage
+ */
+class S3Test extends \PHPUnit_Framework_TestCase {
+    /**
+     * Test that we don't get an exception with required parameters present
+     */
+    public function testConstructorWithAllRequiredParameters() {
+        $s3 = new S3([
+            'key' => 'foo',
+            'secret' => 'bar',
+            'bucket' => 'spam',
+            'region' => 'eggs',
+        ]);
+
+        $this->assertNotNull($s3);
+    }
+
+    /**
+     * Test that we _do_ get an exception with required parameters present
+     *
+     * @expectedException Imbo\Exception\ConfigurationException
+     */
+    public function testConstructorMissingRequiredParameters() {
+        new S3([
+            'key' => '',
+            'secret' => 'bar',
+            'bucket' => '',
+            'region' => 'eggs',
+        ]);
+    }
+
+    /**
+     * Test that we _do_ get exceptions for each single missing parameter
+     */
+    public function testConstructorEachMissingRequiredParameters() {
+        $params = [
+            'key' => 'foo',
+            'secret' => 'bar',
+            'bucket' => 'spam',
+            'region' => 'eggs',
+        ];
+
+        foreach ($params as $param => $dummy) {
+            $local = $params;
+            unset($local[$param]);
+            $exception = false;
+
+            try {
+                new S3($local);
+            } catch (ConfigurationException $e) {
+                $exception = true;
+            } finally {
+                $this->assertTrue($exception);
+            }
+        }
+    }
+
+
+}
