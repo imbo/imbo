90ce4b7556fb893c5fee9391b9b2eb082f63cc65
Christer Edvartsen <cogo@starzinger.net>
2016-11-21 10:16:21 +0100

    Add assertion for response reason phrase. Will be moved to imbo/behat-api-extension later

--- tests/behat/features/bootstrap/RESTContext.php
+++ tests/behat/features/bootstrap/RESTContext.php
@@ -9,6 +9,7 @@
  */
 
 use Imbo\BehatApiExtension\Context\ApiContext;
+use Assert\Assertion;
 
 /**
  * REST context for Behat tests
@@ -17,4 +18,17 @@ use Imbo\BehatApiExtension\Context\ApiContext;
  * @package Test suite\Functional tests
  */
 class RESTContext extends ApiContext {
+    /**
+     * Assert HTTP reason phrase
+     *
+     * @param string $phrase Expected HTTP reason phrase
+     * @Then the response reason phrase is :phrase
+     */
+    public function assertResponseReasonPhrase($phrase) {
+        Assertion::same($phrase, $actual = $this->response->getReasonPhrase(), sprintf(
+            'Invalid HTTP reason phrase, expected "%s", got "%s"',
+            $phrase,
+            $actual
+        ));
+    }
 }
