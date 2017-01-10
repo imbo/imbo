cfc651a6dbe3d5982700a8cb8c13cb04c71c4d37
Mats Lindh <mats@lindh.no>
2017-01-10 14:00:46 +0100

    Modify mocks and add exception test for duplicate image identifiers

--- tests/phpunit/unit/Resource/ImagesTest.php
+++ tests/phpunit/unit/Resource/ImagesTest.php
@@ -11,6 +11,7 @@
 namespace ImboUnitTest\Resource;
 
 use Imbo\Resource\Images,
+    Imbo\Exception\DuplicateImageIdentifierException,
     DateTime,
     DateTimeZone;
 
@@ -31,6 +32,8 @@ class ImagesTest extends ResourceTests {
     private $storage;
     private $manager;
     private $event;
+    private $imageIdentifierGenerator;
+    private $config;
 
     /**
      * {@inheritdoc}
@@ -49,11 +52,15 @@ class ImagesTest extends ResourceTests {
         $this->storage = $this->createMock('Imbo\Storage\StorageInterface');
         $this->event = $this->createMock('Imbo\EventManager\Event');
         $this->manager = $this->createMock('Imbo\EventManager\EventManager');
+        $this->imageIdentifierGenerator = $this->createMock('Imbo\Image\Identifier\Generator\GeneratorInterface');
+        $this->config = ['imageIdentifierGenerator' => $this->imageIdentifierGenerator];
+
         $this->event->expects($this->any())->method('getRequest')->will($this->returnValue($this->request));
         $this->event->expects($this->any())->method('getResponse')->will($this->returnValue($this->response));
         $this->event->expects($this->any())->method('getDatabase')->will($this->returnValue($this->database));
         $this->event->expects($this->any())->method('getStorage')->will($this->returnValue($this->storage));
         $this->event->expects($this->any())->method('getManager')->will($this->returnValue($this->manager));
+        $this->event->expects($this->any())->method('getConfig')->will($this->returnValue($this->config));
 
         $this->resource = $this->getNewResource();
     }
@@ -68,18 +75,46 @@ class ImagesTest extends ResourceTests {
         $this->storage = null;
         $this->event = null;
         $this->manager = null;
+        $this->imageIdentifierGenerator = null;
+        $this->config = null;
     }
 
     /**
      * @covers Imbo\Resource\Images::addImage
      */
     public function testSupportsHttpPost() {
-        $this->manager->expects($this->at(0))->method('trigger')->with('db.image.insert');
+        $this->imageIdentifierGenerator->expects($this->any())->method('isDeterministic')->will($this->returnValue(false));
+        $this->manager->expects($this->at(0))->method('trigger')->with('db.image.insert', ['updateIfDuplicate' => false]);
         $this->manager->expects($this->at(1))->method('trigger')->with('storage.image.insert');
+
         $image = $this->createMock('Imbo\Model\Image');
         $image->expects($this->once())->method('getImageIdentifier')->will($this->returnValue('id'));
+
         $this->request->expects($this->once())->method('getImage')->will($this->returnValue($image));
         $this->response->expects($this->once())->method('setModel')->with($this->isInstanceOf('Imbo\Model\ArrayModel'));
+
+        $this->resource->addImage($this->event);
+    }
+
+    /**
+     * @covers Imbo\Resource\Images::addImage
+     * @expectedException Imbo\Exception\ImageException
+     * @expectedExceptionMessage Failed to generate unique image identifier
+     * @expectedExceptionCode 503
+     */
+    public function testThrowsExceptionWhenItFailsToGenerateUniqueImageIdentifier() {
+        $this->manager->expects($this->any())->method('trigger')->with('db.image.insert', ['updateIfDuplicate' => false])->will($this->throwException(new DuplicateImageIdentifierException()));
+
+        $headers = $this->createMock('Symfony\Component\HttpFoundation\ResponseHeaderBag');
+        $headers->expects($this->once())->method('set')->with('Retry-After', 1);
+        $this->response->headers = $headers;
+
+        $image = $this->createMock('Imbo\Model\Image');
+
+        $this->imageIdentifierGenerator->expects($this->any())->method('isDeterministic')->will($this->returnValue(false));
+        $this->imageIdentifierGenerator->expects($this->any())->method('generate')->will($this->returnValue('foo'));
+
+        $this->request->expects($this->once())->method('getImage')->will($this->returnValue($image));
 
         $this->resource->addImage($this->event);
     }
