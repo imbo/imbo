6623a32eb55aa58090ed4571956e38a75dd369ca
Christer Edvartsen <cogo@starzinger.net>
2017-02-25 15:17:48 +0100

    Remove methods that is now implemented in imbo/behat-api-extension

--- tests/behat/features/bootstrap/RESTContext.php
+++ tests/behat/features/bootstrap/RESTContext.php
@@ -19,49 +19,6 @@ use Assert\Assertion;
  */
 class RESTContext extends ApiContext {
     /**
-     * Assert HTTP reason phrase
-     *
-     * @param string $phrase Expected HTTP reason phrase
-     * @Then the response reason phrase is :phrase
-     */
-    public function assertResponseReasonPhrase($phrase) {
-        Assertion::same($phrase, $actual = $this->response->getReasonPhrase(), sprintf(
-            'Invalid HTTP reason phrase, expected "%s", got "%s"',
-            $phrase,
-            $actual
-        ));
-    }
-
-    /**
-     * Set the request body to a string or a resource
-     *
-     * @param string $content The content to set as the request body. If the string is in fact a
-     *                        path to a file, the resource will be attached instead of the literal
-     *                        string. If a resource is used the step will also figure out the mime
-     *                        type of the file and set the Content-Type header.
-     * @param boolean $forceString If the " as a string" part is used in the step, the content is
-     *                             treated as a string even if it might be a valid path
-     * @Given /^the request body contains "([^"]+)"( as a string)?$/
-     */
-    public function setRequestBody($content, $forceString = false) {
-        $forceString = (boolean) $forceString;
-
-        if (!$forceString && file_exists($content)) {
-            // Set the Content-Type request header
-            $this->requestOptions['headers']['Content-Type'] = mime_content_type($content);
-
-            // Create a resource to the file
-            $content = fopen($filename = $content, 'r');
-
-            if ($content === false) {
-                throw new InvalidArgumentException(sprintf('Could not open "%s" for reading.', $filename));
-            }
-        }
-
-        $this->requestOptions['body'] = $content;
-    }
-
-    /**
      * Check the size of the response body (not the Content-Length response header)
      *
      * @param int $expetedSize The size we are expecting
