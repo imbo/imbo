f09bbb32922b7ce4e7200c6d08f52bb03b62cd71
Christer Edvartsen <cogo@starzinger.net>
2017-02-26 18:24:06 +0100

    Moved remaining methods from RESTContext to FeatureContext, and removed RESTContext.

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -8,6 +8,7 @@
  * distributed with this source code.
  */
 
+use Imbo\BehatApiExtension\Context\ApiContext;
 use Behat\Behat\Hook\Scope\BeforeFeatureScope;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Gherkin\Node\PyStringNode;
@@ -15,6 +16,7 @@ use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Psr7\Uri;
 use GuzzleHttp\Middleware;
 use Psr\Http\Message\RequestInterface;
+use Behat\Gherkin\Node\TableNode;
 use Assert\Assertion;
 
 /**
@@ -23,7 +25,7 @@ use Assert\Assertion;
  * @author Christer Edvartsen <cogo@starzinger.net>
  * @package Test suite\Functional tests
  */
-class FeatureContext extends RESTContext {
+class FeatureContext extends ApiContext {
     const MIDDLEWARE_SIGN_REQUEST = 'sign-request';
     const MIDDLEWARE_APPEND_ACCESS_TOKEN = 'append-access-token';
 
@@ -864,5 +866,65 @@ class FeatureContext extends RESTContext {
             'color' => $hexColor,
             'alpha' => (float) $pixel->getColorValue(\Imagick::COLOR_ALPHA),
         ];
+    }
+
+    /**
+     * Check the size of the response body (not the Content-Length response header)
+     *
+     * @param int $expetedSize The size we are expecting
+     * @Then the response body size is :expectedSize
+     */
+    public function assertResponseBodySize($expectedSize) {
+        $this->requireResponse();
+
+        Assertion::same(
+            $actualSize = strlen((string) $this->response->getBody()),
+            (int) $expectedSize,
+            sprintf('Expected response body size: %d, actual: %d', $expectedSize, $actualSize)
+        );
+    }
+
+    /**
+     * Set a query string parameter
+     *
+     * @param string $name The name of the parameter
+     * @param mixed $value The value for the parameter
+     * @Given the query string parameter :name is set to :value
+     */
+    public function setRequestQueryParameter($name, $value) {
+        if (!is_array($this->requestOptions['query'])) {
+            $this->requestOptions['query'] = [];
+        }
+
+        $this->requestOptions['query'][$name] = $value;
+    }
+
+    /**
+     * Set multiple query string parameters
+     *
+     * @param TableNode $table Query parameters
+     * @Given the following query string parameters are set:
+     */
+    public function setRequestQueryParameters(TableNode $table) {
+        foreach ($table as $row) {
+            $this->addRequestQueryParameter($row['name'], $row['value']);
+        }
+    }
+
+    /**
+     * Set an array-type query parameter
+     *
+     * @param string $name The name of the parameter
+     * @param TableNode $table Values for the query parameter
+     * @Given the query string parameter :name has the following values:
+     */
+    public function setRequestQueryParameterValues($name, TableNode $table) {
+        $values = [];
+
+        foreach ($table as $row) {
+            $values[] = $row['value'];
+        }
+
+        $this->addRequestQueryParameter($name, $values);
     }
 }
