c913a499cd845d2305b41a6497f2c03ebf700817
Christer Edvartsen <cogo@starzinger.net>
2017-06-23 14:31:02 +0200

    Enable autoloading of Behat context classes
    Instead of adding the path of the Behat feature context classes to the
    Behat configuration file, and manually including the files in the
    bootstrap for the unit tests the directory is added to autoload-dev in
    composer.json

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -10,6 +10,3 @@
 
 // Set the Fixtures path
 define('FIXTURES_DIR', realpath(__DIR__ . '/Fixtures'));
-
-// Require the FeatureContext file as it's not part of the regular autolading functionality
-require __DIR__ . '/../behat/features/bootstrap/FeatureContext.php';
