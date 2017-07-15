a28cabc790c18a6f2c27f599a169e6bf64b78327
Christer Edvartsen <cogo@starzinger.net>
2017-07-15 12:26:49 +0200

    Use setup file for SQL
    Instead of duplicating the SQL for creating the tables in the database
    the test classes now use the contents of the file found in the setup dir,
    which is also included in the documentation.

--- tests/behat/features/bootstrap/DatabaseTest/DoctrineSQLite.php
+++ tests/behat/features/bootstrap/DatabaseTest/DoctrineSQLite.php
@@ -28,48 +28,10 @@ class DoctrineSQLite implements AdapterTest {
 
         // Create tmp tables
         $pdo = new PDO(sprintf('sqlite:%s', $path));
-        $pdo->query("
-            CREATE TABLE IF NOT EXISTS imageinfo (
-                id INTEGER PRIMARY KEY NOT NULL,
-                user TEXT NOT NULL,
-                imageIdentifier TEXT NOT NULL,
-                size INTEGER NOT NULL,
-                extension TEXT NOT NULL,
-                mime TEXT NOT NULL,
-                added INTEGER NOT NULL,
-                updated INTEGER NOT NULL,
-                width INTEGER NOT NULL,
-                height INTEGER NOT NULL,
-                checksum TEXT NOT NULL,
-                originalChecksum TEXT NOT NULL,
-                UNIQUE (user,imageIdentifier)
-            )
-        ");
-        $pdo->query("
-            CREATE TABLE IF NOT EXISTS metadata (
-                id INTEGER PRIMARY KEY NOT NULL,
-                imageId KEY INTEGER NOT NULL,
-                tagName TEXT NOT NULL,
-                tagValue TEXT NOT NULL
-            )
-        ");
-        $pdo->query("
-            CREATE TABLE IF NOT EXISTS shorturl (
-                shortUrlId TEXT PRIMARY KEY NOT NULL,
-                user TEXT NOT NULL,
-                imageIdentifier TEXT NOT NULL,
-                extension TEXT,
-                query TEXT NOT NULL
-            )
-        ");
-        $pdo->query("
-            CREATE INDEX shorturlparams ON shorturl (
-                user,
-                imageIdentifier,
-                extension,
-                query
-            )
-        ");
+
+        array_map(function($query) use ($pdo) {
+            $pdo->query($query);
+        }, explode("\n\n", file_get_contents(__DIR__ . '/../../../../../setup/doctrine.sqlite.sql')));
 
         return ['path' => $path];
     }
--- tests/phpunit/integration/Database/DoctrineTest.php
+++ tests/phpunit/integration/Database/DoctrineTest.php
@@ -93,48 +93,10 @@ class DoctrineTest extends DatabaseTests {
 
         // Create tmp tables
         $this->pdo = new PDO(sprintf('sqlite:%s', $this->dbPath));
-        $this->pdo->query("
-            CREATE TABLE IF NOT EXISTS imageinfo (
-                id INTEGER PRIMARY KEY NOT NULL,
-                user TEXT NOT NULL,
-                imageIdentifier TEXT NOT NULL,
-                size INTEGER NOT NULL,
-                extension TEXT NOT NULL,
-                mime TEXT NOT NULL,
-                added INTEGER NOT NULL,
-                updated INTEGER NOT NULL,
-                width INTEGER NOT NULL,
-                height INTEGER NOT NULL,
-                checksum TEXT NOT NULL,
-                originalChecksum TEXT NOT NULL,
-                UNIQUE (user,imageIdentifier)
-            )
-        ");
-        $this->pdo->query("
-            CREATE TABLE IF NOT EXISTS metadata (
-                id INTEGER PRIMARY KEY NOT NULL,
-                imageId KEY INTEGER NOT NULL,
-                tagName TEXT NOT NULL,
-                tagValue TEXT NOT NULL
-            )
-        ");
-        $this->pdo->query("
-            CREATE TABLE IF NOT EXISTS shorturl (
-                shortUrlId TEXT PRIMARY KEY NOT NULL,
-                user TEXT NOT NULL,
-                imageIdentifier TEXT NOT NULL,
-                extension TEXT,
-                query TEXT NOT NULL
-            )
-        ");
-        $this->pdo->query("
-            CREATE INDEX shorturlparams ON shorturl (
-                user,
-                imageIdentifier,
-                extension,
-                query
-            )
-        ");
+
+        array_map(function($query) {
+            $this->pdo->query($query);
+        }, explode("\n\n", file_get_contents(__DIR__ . '/../../../../setup/doctrine.sqlite.sql')));
 
         parent::setUp();
     }
