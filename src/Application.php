8e529f31c9d5cd701449c9aad13eb8cb0f37cd20
Mats Lindh <mats@lindh.no>
2017-09-08 16:36:39 +0200

    Add the basic loader and basic output converter

--- src/Application.php
+++ src/Application.php
@@ -102,6 +102,8 @@ class Application {
             $loaderManager->addLoaders($config['loaders']);
         }
 
+        $loaderManager->registerLoader(new Image\Loader\Basic());
+
         // Create a output conversion manager and register any converters
         $outputConverterManager = new OutputConverterManager();
 
@@ -110,6 +112,8 @@ class Application {
         } else if (isset($config['outputConverters']) && is_array($config['outputConverters'])) {
             $outputConverterManager->addConverters($config['outputConverters']);
         }
+
+        $outputConverterManager->registerConverter(new Image\OutputConverter\Basic());
 
         // Create the event manager and the event template
         $eventManager = new EventManager();
