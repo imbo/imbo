f447bfac5e4198b1e976dbe4fefd663dd228b99f
Mats Lindh <mats@lindh.no>
2017-09-11 10:43:53 +0200

    Add optional Tiff loader support

---
+++ src/Image/Loader/Tiff.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace Imbo\Image\Loader;
+
+/**
+ * TIFF image loader
+ *
+ * @author Mats Lindh <mats@lindh.no>
+ * @package Image\Loaders
+ */
+class Tiff implements LoaderInterface {
+    public function getMimeTypeCallbacks() {
+        return [
+            'image/tiff' => [
+                'extension' => 'tif',
+                'callback' => [$this, 'load'],
+            ],
+        ];
+    }
+
+    public function load($blob) {
+        $imagick = new \Imagick();
+        $imagick->readImageBlob($blob);
+        return $imagick;
+    }
+}
