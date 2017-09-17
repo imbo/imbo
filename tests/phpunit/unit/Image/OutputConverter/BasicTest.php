fe2792633a857472aa0d5528948a6d8ec8aa6496
Christer Edvartsen <cogo@starzinger.net>
2017-09-17 22:22:59 +0200

    Add unit tests for output converters

---
+++ tests/phpunit/unit/Image/OutputConverter/BasicTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace ImboUnitTest\Image\OutputConverter;
+
+use Imbo\Image\OutputConverter\Basic;
+use PHPUnit_Framework_TestCase;
+use ImagickException;
+
+/**
+ * @coversDefaultClass Imbo\Image\OutputConverter\Basic
+ */
+class BasicTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @var Basic
+     */
+    private $converter;
+
+    /**
+     * Set up the loader
+     */
+    public function setup() {
+        $this->converter = new Basic();
+    }
+
+    /**
+     * @covers ::getSupportedMimeTypes
+     */
+    public function testReturnsSupportedMimeTypes() {
+        $types = $this->converter->getSupportedMimeTypes();
+
+        $this->assertInternalType('array', $types);
+
+        $this->assertContains('image/png', array_keys($types));
+        $this->assertContains('image/jpeg', array_keys($types));
+        $this->assertContains('image/gif', array_keys($types));
+    }
+
+    /**
+     * @covers ::convert
+     */
+    public function testCanConvertImage() {
+        $extension = 'png';
+        $mimeType = 'image/png';
+
+        $imagick = $this->createMock('Imagick');
+        $imagick->expects($this->once())
+                ->method('setImageFormat')
+                ->with($extension);
+
+        $image = $this->createMock('Imbo\Model\Image');
+        $image->expects($this->once())
+              ->method('hasBeenTransformed')
+              ->with(true);
+
+        $this->assertNull($this->converter->convert($imagick, $image, $extension, $mimeType));
+    }
+
+    /**
+     * @covers ::convert
+     * @expectedException Imbo\Exception\OutputConverterException
+     * @expectedExceptionMessage some error
+     * @expectedExceptionCode 400
+     */
+    public function testThrowsExceptionOnImagickFailure() {
+        $extension = 'png';
+
+        $imagick = $this->createMock('Imagick');
+        $imagick->expects($this->once())
+                ->method('setImageFormat')
+                ->with($extension)
+                ->will($this->throwException(new ImagickException('some error')));
+
+        $this->assertNull($this->converter->convert($imagick, $this->createMock('Imbo\Model\Image'), $extension, 'image/png'));
+    }
+}
---
+++ tests/phpunit/unit/Image/OutputConverter/WebpTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace ImboUnitTest\Image\OutputConverter;
+
+use Imbo\Image\OutputConverter\Webp;
+use PHPUnit_Framework_TestCase;
+use ImagickException;
+
+/**
+ * @coversDefaultClass Imbo\Image\OutputConverter\Webp
+ */
+class WebpTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @var Webp
+     */
+    private $converter;
+
+    /**
+     * Set up the loader
+     */
+    public function setup() {
+        $this->converter = new Webp();
+    }
+
+    /**
+     * @covers ::getSupportedMimeTypes
+     */
+    public function testReturnsSupportedMimeTypes() {
+        $types = $this->converter->getSupportedMimeTypes();
+
+        $this->assertInternalType('array', $types);
+
+        $this->assertContains('image/webp', array_keys($types));
+    }
+
+    /**
+     * @covers ::convert
+     */
+    public function testCanConvertImage() {
+        $extension = 'webp';
+        $mimeType = 'image/webp';
+
+        $imagick = $this->createMock('Imagick');
+        $imagick->expects($this->once())
+                ->method('setImageFormat')
+                ->with($extension);
+
+        $image = $this->createMock('Imbo\Model\Image');
+        $image->expects($this->once())
+              ->method('hasBeenTransformed')
+              ->with(true);
+
+        $this->assertNull($this->converter->convert($imagick, $image, $extension, $mimeType));
+    }
+
+    /**
+     * @covers ::convert
+     * @expectedException Imbo\Exception\OutputConverterException
+     * @expectedExceptionMessage some error
+     * @expectedExceptionCode 400
+     */
+    public function testThrowsExceptionOnImagickFailure() {
+        $extension = 'webp';
+
+        $imagick = $this->createMock('Imagick');
+        $imagick->expects($this->once())
+                ->method('setImageFormat')
+                ->with($extension)
+                ->will($this->throwException(new ImagickException('some error')));
+
+        $this->assertNull($this->converter->convert($imagick, $this->createMock('Imbo\Model\Image'), $extension, 'image/webp'));
+    }
+}
