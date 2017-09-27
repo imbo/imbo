34a82bf931633f13216db1d5e9169684992867ea
Mats Lindh <mats@lindh.no>
2017-09-27 16:31:40 +0200

    Support named clipping paths

--- src/Image/Transformation/Clip.php
+++ src/Image/Transformation/Clip.php
@@ -24,9 +24,24 @@ class Clip extends Transformation {
      * {@inheritdoc}
      */
     public function transform(array $params) {
+        $pathName = null;
+
+        if (!empty($params['name'])) {
+            $pathName = $params['name'];
+        }
+
         try {
             $this->imagick->setImageAlphaChannel(\Imagick::ALPHACHANNEL_TRANSPARENT);
-            $this->imagick->clipImage();
+
+            // since the implementation of clipImage in ImageMagick is
+            //     return(ClipImagePath(image,"#1",MagickTrue,exception));
+            // .. this should be the same by setting inside=true.
+            if ($pathName) {
+                $this->imagick->clipImagePath($pathName, true);
+            } else {
+                $this->imagick->clipImage();
+            }
+
             $this->imagick->setImageAlphaChannel(\Imagick::ALPHACHANNEL_OPAQUE);
         } catch (ImagickException $e) {
             // NoClipPathDefined - the image doesn't have a clip path, but this isn't an fatal error.
