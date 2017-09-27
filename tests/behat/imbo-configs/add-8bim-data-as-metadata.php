36470ae70674c11563e51e9397dcf34095dc1225
Mats Lindh <mats@lindh.no>
2017-09-27 14:08:37 +0200

    Add 8BIM profile path extraction event listener
    This event listener extracts paths from the 8BIM metadata and adds them
    as metadata under the "paths" element in the metadata store.
    
    It currently only extracts the paths, but if necessary in the future
    can be expanded to also extract and parse additional fields. In that
    cases we should consider a separate "8bim:" namespace for metadata.

---
+++ tests/behat/imbo-configs/add-8bim-data-as-metadata.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+/**
+ * Enable the EXIF metadata listener
+ */
+return [
+    'eventListeners' => [
+        '8BIMMetadataListener' => [
+            'listener' => 'Imbo\EventListener\EightbimMetadata',
+        ]
+    ],
+];
---
+++ tests/phpunit/unit/EventListener/EightbimMetadataTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file is part of the Imbo package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace ImboUnitTest\EventListener;
+
+use Imbo\EventListener\EightbimMetadata;
+
+/**
+ * @covers Imbo\EventListener\ExifMetadata
+ * @group unit
+ * @group listeners
+ */
+class EightbimMetadataTest extends ListenerTests {
+    /**
+     * Set up the listener
+     */
+    public function setUp() {
+        $this->listener = new EightbimMetadata();
+    }
+
+    /**
+     * Tear down the listener
+     */
+    public function tearDown() {
+        $this->listener = null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getListener() {
+        return $this->listener;
+    }
+
+    public function testCanFilterData() {
+        $user = 'user';
+        $imageIdentifier = 'imageIdentifier';
+        $blob = file_get_contents(FIXTURES_DIR . '/jpeg-with-multiple-paths.jpg');
+
+        $image = $this->createMock('Imbo\Model\Image');
+        $image->expects($this->once())->method('getImageIdentifier')->will($this->returnValue($imageIdentifier));
+        $image->expects($this->once())->method('getBlob')->will($this->returnValue($blob));
+
+        $request = $this->createMock('Imbo\Http\Request\Request');
+        $request->expects($this->once())->method('getUser')->will($this->returnValue($user));
+        $request->expects($this->any())->method('getImage')->will($this->returnValue($image));
+
+        $database = $this->createMock('Imbo\Database\DatabaseInterface');
+        $database->expects($this->once())->method('updateMetadata')->with($user, $imageIdentifier, [
+            'paths' => ['House', 'Panda'],
+        ]);
+
+        $event = $this->createMock('Imbo\EventManager\Event');
+        $event->expects($this->exactly(2))->method('getRequest')->will($this->returnValue($request));
+        $event->expects($this->once())->method('getDatabase')->will($this->returnValue($database));
+
+        $listener = new EightbimMetadata();
+        $listener->populate($event);
+        $listener->save($event);
+    }
+}
