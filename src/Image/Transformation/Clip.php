257228673aa1c2faec176c6d3d75a219440cb664
Mats Lindh <mats@lindh.no>
2017-10-02 15:45:53 +0200

    Check clipping path name against metadata and retain existing alpha mode

--- src/Image/Transformation/Clip.php
+++ src/Image/Transformation/Clip.php
@@ -10,11 +10,12 @@
 
 namespace Imbo\Image\Transformation;
 
-use Imbo\Exception\TransformationException,
+use Imbo\Exception\InvalidArgumentException,
+    Imbo\Exception\TransformationException,
     ImagickException;
 
 /**
- * Clip transformation
+ * Clip transformation for making an image transparent outside of a clipping mask
  *
  * @author Mats Lindh <mats@lindh.no>
  * @package Image\Transformations
@@ -28,7 +29,22 @@ class Clip extends Transformation {
 
         if (!empty($params['name'])) {
             $pathName = $params['name'];
+
+            $metadata = $this->event->getDatabase()->getMetadata(
+                $this->image->getUser(),
+                $this->image->getImageIdentifier()
+            );
+
+            if (empty($metadata['paths']) || !is_array($metadata['paths']) || !in_array($pathName, $metadata['paths'])) {
+                if (isset($params['ignoreUnknownPath'])) {
+                    return;
+                }
+
+                throw new InvalidArgumentException('Selected clipping path "' . $pathName . '" was not found in the image. Add the ignoreUnknownPath argument if you want to ignore this error.', 400);
+            }
         }
+
+        $currentAlphaChannelMode = $this->imagick->getImageAlphaChannel();
 
         try {
             $this->imagick->setImageAlphaChannel(\Imagick::ALPHACHANNEL_TRANSPARENT);
@@ -44,8 +60,10 @@ class Clip extends Transformation {
 
             $this->imagick->setImageAlphaChannel(\Imagick::ALPHACHANNEL_OPAQUE);
         } catch (ImagickException $e) {
-            // NoClipPathDefined - the image doesn't have a clip path, but this isn't an fatal error.
+            // NoClipPathDefined - the image doesn't have a clipping path, but this isn't a fatal error.
             if ($e->getCode() == 410) {
+                // but we need to reset the alpha channel mode in case someone else is doing something with it
+                $this->imagick->setImageAlphaChannel($currentAlphaChannelMode);
                 return;
             }
 
