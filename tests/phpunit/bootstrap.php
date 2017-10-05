35bcc536953c1ef8a5fbd93c0506aa2ff73e9e40
Mats Lindh <mats@lindh.no>
2017-10-05 11:05:57 +0200

    Add DATA_DIR to test fixture configuration

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -11,3 +11,4 @@
 // Set the Fixtures path
 define('FIXTURES_DIR', realpath(__DIR__ . '/Fixtures'));
 define('PROJECT_ROOT', realpath(dirname(dirname(__DIR__))));
+define('DATA_DIR', PROJECT_ROOT . '/data');
