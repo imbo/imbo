eadc68e17029db9e70afb0b3ba5dc3d380aff90d
Christer Edvartsen <cogo@starzinger.net>
2019-09-18 08:18:58 +0200

    Started cleaning up tests

--- tests/phpunit/unit/EventManager/EventTest.php
+++ tests/phpunit/unit/EventManager/EventTest.php
@@ -1,45 +1,56 @@
-<?php
-namespace ImboUnitTest\EventManager;
+<?php declare(strict_types=1);
+namespace Imbo\EventManager;
 
-use Imbo\EventManager\Event;
+use Imbo\Http\Request\Request;
+use Imbo\Http\Response\Response;
+use Imbo\Database\DatabaseInterface;
+use Imbo\Storage\StorageInterface;
+use Imbo\Auth\AccessControl\Adapter\AdapterInterface;
+use Imbo\EventManager\EventManager;
 use Imbo\Exception\InvalidArgumentException;
+use Imbo\Image\InputLoaderManager;
+use Imbo\Image\OutputConverter\OutputConverterInterface;
+use Imbo\Image\TransformationManager;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @coversDefaultClass Imbo\EventManager\Event
  */
 class EventTest extends TestCase {
-    /**
-     * @var Event
-     */
     private $event;
 
-    /**
-     * Set up the event instance
-     */
     public function setUp() : void {
         $this->event = new Event();
     }
 
-    public function getArguments() {
+    public function getArguments() : array {
         return [
             'request' => [
-                'getRequest', 'request', $this->createMock('Imbo\Http\Request\Request'),
+                'getRequest', 'request', $this->createMock(Request::class),
             ],
             'response' => [
-                'getResponse', 'response', $this->createMock('Imbo\Http\Response\Response'),
+                'getResponse', 'response', $this->createMock(Response::class),
             ],
             'database' => [
-                'getDatabase', 'database', $this->createMock('Imbo\Database\DatabaseInterface'),
+                'getDatabase', 'database', $this->createMock(DatabaseInterface::class),
             ],
             'storage' => [
-                'getStorage', 'storage', $this->createMock('Imbo\Storage\StorageInterface'),
+                'getStorage', 'storage', $this->createMock(StorageInterface::class),
             ],
             'accessControl' => [
-                'getAccessControl', 'accessControl', $this->createMock('Imbo\Auth\AccessControl\Adapter\AdapterInterface'),
+                'getAccessControl', 'accessControl', $this->createMock(AdapterInterface::class),
             ],
             'manager' => [
-                'getManager', 'manager', $this->createMock('Imbo\EventManager\EventManager'),
+                'getManager', 'manager', $this->createMock(EventManager::class),
+            ],
+            'transformationManager' => [
+                'getTransformationManager', 'transformationManager', $this->createMock(TransformationManager::class),
+            ],
+            'inputLoaderManager' => [
+                'getInputLoaderManager', 'inputLoaderManager', $this->createMock(InputLoaderManager::class),
+            ],
+            'outputConverterManager' => [
+                'getOutputConverterManager', 'outputConverterManager', $this->createMock(OutputConverterInterface::class),
             ],
             'config' => [
                 'getConfig', 'config', ['some' => 'config'],
@@ -52,15 +63,27 @@ class EventTest extends TestCase {
 
     /**
      * @dataProvider getArguments
+     * @covers ::setArgument
+     * @covers ::getRequest
+     * @covers ::getResponse
+     * @covers ::getDatabase
+     * @covers ::getStorage
+     * @covers ::getAccessControl
+     * @covers ::getManager
+     * @covers ::getConfig
+     * @covers ::getHandler
+     * @covers ::getTransformationManager
+     * @covers ::getInputLoaderManager
+     * @covers ::getOutputConverterManager
      */
-    public function testCanSetAndGetRequest($method, $argument, $value) : void {
+    public function testCanSetAndGetRequest(string $method, string $argument, $value) : void {
         $this->event->setArgument($argument, $value);
         $this->assertSame($value, $this->event->$method());
     }
 
     /**
-     * @covers Imbo\EventManager\Event::setName
-     * @covers Imbo\EventManager\Event::getName
+     * @covers ::setName
+     * @covers ::getName
      */
     public function testCanSetAndGetName() : void {
         $this->assertNull($this->event->getName());
@@ -69,8 +92,8 @@ class EventTest extends TestCase {
     }
 
     /**
-     * @covers Imbo\EventManager\Event::stopPropagation
-     * @covers Imbo\EventManager\Event::isPropagationStopped
+     * @covers ::stopPropagation
+     * @covers ::isPropagationStopped
      */
     public function testCanStopPropagation() : void {
         $this->assertFalse($this->event->isPropagationStopped());
@@ -79,7 +102,7 @@ class EventTest extends TestCase {
     }
 
     /**
-     * @covers Imbo\EventManager\Event::getArgument
+     * @covers ::getArgument
      */
     public function testThrowsExceptionWhenGettingArgumentThatDoesNotExist() : void {
         $this->expectExceptionObject(new InvalidArgumentException(
@@ -90,18 +113,20 @@ class EventTest extends TestCase {
     }
 
     /**
-     * @covers Imbo\EventManager\Event::__construct
-     * @covers Imbo\EventManager\Event::setArguments
+     * @covers ::__construct
+     * @covers ::setArguments
      */
     public function testCanSetArgumentsThroughConstructor() : void {
-        $event = new Event(['foo' => 'bar']);
-        $this->assertSame('bar', $event->getArgument('foo'));
+        $this->assertSame(
+            'bar',
+            (new Event(['foo' => 'bar']))->getArgument('foo')
+        );
     }
 
     /**
-     * @covers Imbo\EventManager\Event::setArguments
-     * @covers Imbo\EventManager\Event::getArgument
-     * @covers Imbo\EventManager\Event::hasArgument
+     * @covers ::setArguments
+     * @covers ::getArgument
+     * @covers ::hasArgument
      */
     public function testSetArgumentsOverridesAllArguments() : void {
         $this->assertFalse($this->event->hasArgument('foo'));
