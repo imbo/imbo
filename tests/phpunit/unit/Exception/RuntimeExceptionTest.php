a2c4e14f4cdf0364d029be0d7523465bde70e18b
Christer Edvartsen <cogo@starzinger.net>
2019-09-19 13:07:49 +0200

    Clean up Exception tests

--- tests/phpunit/unit/Exception/RuntimeExceptionTest.php
+++ tests/phpunit/unit/Exception/RuntimeExceptionTest.php
@@ -1,19 +1,13 @@
 <?php declare(strict_types=1);
-namespace ImboUnitTest\Exception;
+namespace Imbo\Exception;
 
-use Imbo\Exception\RuntimeException;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @coversDefaultClass Imbo\Exception\RuntimeException
  */
 class RuntimeExceptionTest extends TestCase {
-    /**
-     * Fetch imbo error codes
-     *
-     * @return array[]
-     */
-    public function getErrorCodes() {
+    public function getErrorCodes() : array {
         return [
             [123, 123],
             ['123', 123],
@@ -22,14 +16,13 @@ class RuntimeExceptionTest extends TestCase {
     }
 
     /**
-     * @covers Imbo\Exception\RuntimeException::setImboErrorCode
-     * @covers Imbo\Exception\RuntimeException::getImboErrorCode
      * @dataProvider getErrorCodes
+     * @covers ::setImboErrorCode
+     * @covers ::getImboErrorCode
      */
-    public function testSetAndGetImboErrorCode($actual, $expected) : void {
+    public function testSetAndGetImboErrorCode($actual, int $expected) : void {
         $exception = new RuntimeException();
         $this->assertSame($exception, $exception->setImboErrorCode($actual));
         $this->assertSame($expected, $exception->getImboErrorCode());
     }
-
 }
