cb761e919d18db2052e2063e2aa672a7af747067
Christer Edvartsen <cogo@starzinger.net>
2019-09-19 13:07:36 +0200

    Clean up CliCommand tests

--- tests/phpunit/unit/CliCommand/CliCommandTest.php
+++ tests/phpunit/unit/CliCommand/CliCommandTest.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1);
-namespace ImboUnitTest\CliCommand;
+namespace Imbo\CliCommand;
 
 use PHPUnit\Framework\TestCase;
 
@@ -7,25 +7,37 @@ use PHPUnit\Framework\TestCase;
  * @coversDefaultClass Imbo\CliCommand\CliCommand
  */
 class CliCommandTest extends TestCase {
-    /**
-     * @var Imbo\CliCommand\CliCommand
-     */
     private $command;
 
-    /**
-     * Set up the command
-     */
     public function setUp() : void {
-        $this->command = $this->getMockBuilder('Imbo\CliCommand\CliCommand')->disableOriginalConstructor()->getMockForAbstractClass();
+        $this->command = $this
+            ->getMockBuilder(CliCommand::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
     }
 
+    /**
+     * @covers ::getConfig
+     */
     public function testCanSetAndGetConfiguration() : void {
         $config = ['some' => 'config'];
         $this->command->setConfig($config);
         $this->assertSame($config, $this->command->getConfig());
     }
 
+    /**
+     * @covers ::getConfig
+     */
     public function testFetchesTheDefaultConfigurationIfNoneHasBeenSet() : void {
         $this->assertEquals(require __DIR__ . '/../../../../config/config.default.php', $this->command->getConfig());
+    }
+
+    /**
+     * @covers ::getConfig
+     * @covers ::setConfig
+     */
+    public function testCanSetConfig() : void {
+        $this->command->setConfig(['some' => 'config']);
+        $this->assertSame(['some' => 'config'], $this->command->getConfig());
     }
 }
