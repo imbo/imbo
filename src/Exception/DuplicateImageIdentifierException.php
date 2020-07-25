59e7367d752c7964f8a3cf676da18b9d99f160ec
Christer Edvartsen <cogo@starzinger.net>
2020-07-25 19:04:57 +0200

    Exception should extend database exception

--- src/Exception/DuplicateImageIdentifierException.php
+++ src/Exception/DuplicateImageIdentifierException.php
@@ -4,4 +4,4 @@ namespace Imbo\Exception;
 /**
  * Duplicate Image Identifier exception - thrown if the image identifier already exists in the underlying database.
  */
-class DuplicateImageIdentifierException extends RuntimeException {}
+class DuplicateImageIdentifierException extends DatabaseException {}
