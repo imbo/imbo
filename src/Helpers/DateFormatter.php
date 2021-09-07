8c62154a9397d098a3b087584585f12de8d100bf
Christer Edvartsen <cogo@starzinger.net>
2021-09-07 21:09:54 +0200

    Coding standard fix

--- src/Helpers/DateFormatter.php
+++ src/Helpers/DateFormatter.php
@@ -1,22 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 namespace Imbo\Helpers;
 
 use DateTime;
 use DateTimeZone;
 
-/**
- * Date formatter helper class
- */
-class DateFormatter {
+class DateFormatter
+{
     /**
      * Get a formatted date
      *
      * @param DateTime $date An instance of DateTime
      * @return string Returns a formatted date string
      */
-    public function formatDate(DateTime $date) {
-        $date->setTimezone(new DateTimeZone('UTC'));
-
-        return $date->format('D, d M Y H:i:s') . ' GMT';
+    public function formatDate(DateTime $date): string
+    {
+        return $date
+            ->setTimezone(new DateTimeZone('UTC'))
+            ->format('D, d M Y H:i:s') . ' GMT';
     }
 }
--- tests/Helpers/DateFormatterTest.php
+++ tests/Helpers/DateFormatterTest.php
@@ -1,20 +1,26 @@
 <?php declare(strict_types=1);
 namespace Imbo\Helpers;
 
-use PHPUnit\Framework\TestCase;
 use DateTime;
+use PHPUnit\Framework\TestCase;
 
 /**
  * @coversDefaultClass Imbo\Helpers\DateFormatter
  */
-class DateFormatterTest extends TestCase {
-    private $helper;
+class DateFormatterTest extends TestCase
+{
+    private DateFormatter $helper;
 
-    public function setUp() : void {
+    public function setUp(): void
+    {
         $this->helper = new DateFormatter();
     }
 
-    public function getDates() : array {
+    /**
+     * @return array<array{0:DateTime,1:string}>
+     */
+    public function getDates(): array
+    {
         return [
             [new DateTime('@1234567890'), 'Fri, 13 Feb 2009 23:31:30 GMT'],
             [new DateTime('16/Mar/2012:15:05:00 +0100'), 'Fri, 16 Mar 2012 14:05:00 GMT'],
@@ -25,7 +31,8 @@ class DateFormatterTest extends TestCase {
      * @dataProvider getDates
      * @covers ::formatDate
      */
-    public function testCanFormatADateTimeInstance($datetime, $expected) : void {
+    public function testCanFormatADateTimeInstance(DateTime $datetime, string $expected): void
+    {
         $this->assertSame($expected, $this->helper->formatDate($datetime));
     }
 }
