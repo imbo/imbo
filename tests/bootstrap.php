6bd325b73973eec3d2bb1ce747695d4d4d92431b
Christer Edvartsen <cogo@starzinger.net>
2022-04-13 08:43:49 +0200

    Fix incorrect path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,4 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 defined('FIXTURES_DIR') || define('FIXTURES_DIR', __DIR__ . '/Fixtures');
-defined('DATA_DIR') || define('DATA_DIR', __DIR__ . '/../data/profiles');
+defined('DATA_DIR') || define('DATA_DIR', __DIR__ . '/../data');
